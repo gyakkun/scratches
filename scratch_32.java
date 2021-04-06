7f65bc7ceaf381aacf45d3baecad34b797dd0e7f
gyakkun <xiaowufeixia@gmail.com>
2021-04-06 14:23:03 +0800

    new

--- scratch_32.java
+++ scratch_32.java
@@ -9,6 +9,21 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC1143
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+
     // LC781
     public int numRabbits(int[] answers) {
         Map<Integer, Integer> m = new HashMap<>();
