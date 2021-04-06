a916b0b9167acc3e4dd8ebb5b629f36a7d9236b9
gyakkun <xiaowufeixia@gmail.com>
2021-04-06 16:59:31 +0800

    new

---
+++ scratch_34.java
@@ -0,0 +1,136 @@
+import java.util.*;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scratch s = new Scratch();
+        System.err.println(s.clumsy(10000));
+
+    }
+
+    // LC1006
+    public int clumsy(int N) {
+        char[] ops = new char[]{'*', '/', '+', '-'};
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i <N; i++) {
+            sb.append(N - i);
+            sb.append(ops[i % 4]);
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return calculate(sb.toString());
+    }
+
+    public int calculate(String s) {
+        return (int) evalRPN(toRPN(decodeExpression(s)));
+    }
+
+    public List<String> toRPN(List<String> express) {
+        List<String> rpn = new LinkedList<>();
+        Deque<String> stack = new LinkedList<>();
+        Set<String> notNumber = new HashSet<String>() {{
+            add("+");
+            add("-");
+            add("/");
+            add("*");
+            add("(");
+            add(")");
+        }};
+        String tmp;
+        for (String token : express) {
+            if (!notNumber.contains(token)) {
+                rpn.add(token);
+            } else if (token.equals("(")) {
+                stack.push(token);
+            } else if (token.equals(")")) {
+                while (!(tmp = stack.pop()).equals("(")) {
+                    rpn.add(tmp);
+                }
+
+            } else {
+                while (!stack.isEmpty() && getOperPriority(stack.peek()) >= getOperPriority(token)) {
+                    rpn.add(stack.pop());
+                }
+                stack.push(token);
+            }
+        }
+        while (!stack.isEmpty()) {
+            rpn.add(stack.pop());
+        }
+        return rpn;
+    }
+
+    private List<String> decodeExpression(String express) {
+        express = express.replaceAll("\\ ", "");
+        express = express.replaceAll("\\(\\+", "(0+");
+        express = express.replaceAll("\\(\\-", "(0-");
+        express = express.replaceAll("\\((\\d+\\.?\\d*)\\)", "$1");
+        List<String> result = new LinkedList<>();
+        int i = 0;
+        StringBuffer sb;
+        do {
+            if ((express.charAt(i) < '0' || express.charAt(i) > '9') && express.charAt(i) != '.') {
+                result.add(express.charAt(i) + "");
+                i++;
+            } else {
+                sb = new StringBuffer();
+                while (i < express.length() && ((express.charAt(i) >= '0' && express.charAt(i) <= '9') || express.charAt(i) == '.')) {
+                    sb.append(express.charAt(i));
+                    i++;
+                }
+                result.add(sb.toString());
+            }
+        } while (i < express.length());
+        return result;
+    }
+
+    private int getOperPriority(String oper) {
+        switch (oper) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                return -1;
+        }
+    }
+
+    // LC150 逆波兰表达式
+    public int evalRPN(List<String> tokens) {
+        Deque<String> stack = new LinkedList<>();
+        stack.push("0");
+        Set<String> oper = new HashSet<String>() {{
+            add("+");
+            add("-");
+            add("/");
+            add("*");
+        }};
+        for (String token : tokens) {
+            if (oper.contains(token)) {
+                int a = Integer.parseInt(stack.pop());
+                int b = Integer.parseInt(stack.pop());
+                int tmp;
+                switch (token) {
+                    case "+":
+                        tmp = a + b;
+                        break;
+                    case "-":
+                        tmp = b - a;
+                        break;
+                    case "/":
+                        tmp = b / a;
+                        break;
+                    case "*":
+                        tmp = a * b;
+                        break;
+                    default:
+                        tmp = 0;
+                }
+                stack.push(String.valueOf(tmp));
+            } else {
+                stack.push(token);
+            }
+        }
+        return stack.isEmpty() ? 0 : Integer.parseInt(stack.pop());
+    }
+}
