6b66b6034068cc31db2faab085698b1d5a905fd5
gyakkun <xiaowufeixia@gmail.com>
2021-04-06 17:13:51 +0800

    new

--- scratch_32.java
+++ scratch_32.java
@@ -10,6 +10,21 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC32
+    public int longestValidParentheses(String s) {
+        if (s.length() == 0) return 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] - 1 >= 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+            }
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
 
     // LC53
     public int maxSubArray(int[] nums) {
