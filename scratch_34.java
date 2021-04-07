2a058958999f1e9d84153f2dab4ffda222df5cc3
gyakkun <xiaowufeixia@gmail.com>
2021-04-07 11:09:49 +0800

    new

--- scratch_34.java
+++ scratch_34.java
@@ -4,8 +4,63 @@ import java.util.*;
 class Scratch {
     public static void main(String[] args) {
         Scratch s = new Scratch();
-        System.err.println(s.clumsy(10000));
+        System.err.println(s.canJump(new int[]{3, 2, 1, 0, 4}));
+    }
 
+    // LC55
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // LC55 AC
+    public boolean canJumpAC(int[] nums) {
+        boolean[] reachable = new boolean[nums.length];
+        boolean[] visited = new boolean[nums.length];
+        reachable[0] = true;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (!reachable[i]) continue;
+            int j = i + nums[i];
+
+            while (j < nums.length) {
+                if (visited[j]) break;
+                reachable[j] = true;
+                visited[j] = true;
+                j += nums[j];
+                if (nums[j] == 0) break;
+                if (reachable[nums.length - 1]) return true;
+            }
+
+            for (int k = nums[i]; k >= 1; k--) {
+                if (i + k > nums.length) continue;
+                reachable[i + k] = true;
+                if (reachable[nums.length - 1]) return true;
+            }
+        }
+        return reachable[nums.length - 1];
+    }
+
+    // LC55 并查集TLE
+    public boolean canJumpTLE(int[] nums) {
+        DSUArray dsu = new DSUArray();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j <= nums[i]; j++) {
+                dsu.add(i + j);
+                dsu.merge(i, i + j);
+            }
+            if (dsu.isConnected(0, nums.length - 1)) return true;
+        }
+        return dsu.isConnected(0, nums.length - 1);
     }
 
     // LC81
@@ -169,4 +224,156 @@ class Scratch {
         }
         return stack.isEmpty() ? 0 : Integer.parseInt(stack.pop());
     }
+}
+
+class DSUArray {
+    int[] father;
+    int[] rank;
+    int size;
+
+    public DSUArray(int size) {
+        this.size = size;
+        father = new int[size];
+        rank = new int[size];
+        Arrays.fill(father, -1);
+        Arrays.fill(rank, -1);
+    }
+
+    public DSUArray() {
+        this.size = Integer.MAX_VALUE >> 16;
+        father = new int[Integer.MAX_VALUE >> 16];
+        rank = new int[Integer.MAX_VALUE >> 16];
+        Arrays.fill(father, -1);
+        Arrays.fill(rank, -1);
+    }
+
+    public void add(int i) {
+        if (i >= this.size || i < 0) return;
+        if (father[i] == -1) {
+            father[i] = i;
+        }
+        if (rank[i] == -1) {
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int i) {
+        if (i >= this.size || i < 0) return -1;
+        int root = i;
+        while (root < size && root >= 0 && father[root] != root) {
+            root = father[root];
+        }
+        if (root == -1) return -1;
+        while (father[i] != root) {
+            int origFather = father[i];
+            father[i] = root;
+            rank[root]++;
+            i = origFather;
+        }
+        return root;
+    }
+
+    public boolean merge(int i, int j) {
+        if (i >= this.size || i < 0) return false;
+        if (j >= this.size || j < 0) return false;
+        int iFather = find(i);
+        int jFather = find(j);
+        if (iFather == -1 || jFather == -1) return false;
+        if (iFather == jFather) return false;
+
+        if (rank[iFather] >= rank[jFather]) {
+            father[jFather] = iFather;
+            rank[iFather] += rank[jFather];
+        } else {
+            father[iFather] = jFather;
+            rank[jFather] += rank[iFather];
+        }
+        return true;
+    }
+
+    public boolean isConnected(int i, int j) {
+        if (i >= this.size || i < 0) return false;
+        if (i >= this.size || i < 0) return false;
+        return find(i) == find(j);
+    }
+
+}
+
+class DisjointSetUnion {
+
+    Map<Integer, Integer> father;
+    Map<Integer, Integer> rank;
+
+    public DisjointSetUnion() {
+        father = new HashMap<>();
+        rank = new HashMap<>();
+    }
+
+    public void add(int i) {
+        if (!father.containsKey(i)) {
+            // 置初始父亲为自身
+            // 之后判断连通分量个数时候, 遍历father, 找value==key的
+            father.put(i, i);
+        }
+        if (!rank.containsKey(i)) {
+            rank.put(i, 1);
+        }
+    }
+
+    // 找父亲, 路径压缩
+    public int find(int i) {
+        //先找到根 再压缩
+        int root = i;
+        while (father.get(root) != root) {
+            root = father.get(root);
+        }
+        // 找到根, 开始对一路上的子节点进行路径压缩
+        while (father.get(i) != root) {
+            int origFather = father.get(i);
+            father.put(i, root);
+            // 更新秩, 按照节点数
+            rank.put(root, rank.get(root) + 1);
+            i = origFather;
+        }
+        return root;
+    }
+
+    public boolean merge(int i, int j) {
+        int iFather = find(i);
+        int jFather = find(j);
+        if (iFather == jFather) return false;
+        // 按秩合并
+        if (rank.get(iFather) >= rank.get(jFather)) {
+            father.put(jFather, iFather);
+            rank.put(iFather, rank.get(jFather) + rank.get(iFather));
+        } else {
+            father.put(iFather, jFather);
+            rank.put(jFather, rank.get(jFather) + rank.get(iFather));
+        }
+        return true;
+    }
+
+    public boolean isConnected(int i, int j) {
+        return find(i) == find(j);
+    }
+
+    public Map<Integer, Set<Integer>> getAllGroups() {
+        Map<Integer, Set<Integer>> result = new HashMap<>();
+        // 找出所有根
+        for (Integer i : father.keySet()) {
+            int f = find(i);
+            result.putIfAbsent(f, new HashSet<>());
+            result.get(f).add(i);
+        }
+        return result;
+    }
+
+    public int getNumOfGroups() {
+        Set<Integer> s = new HashSet<Integer>();
+        for (Integer i : father.keySet()) {
+            s.add(find(i));
+        }
+        return s.size();
+    }
+
 }
