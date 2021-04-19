3a020e0a72c8f3a9d353dd6a6b2df117b745dae4
gyakkun <xiaowufeixia@gmail.com>
2021-04-19 13:43:47 +0800

    new

--- scratch_36.java
+++ scratch_36.java
@@ -123,6 +123,30 @@ class Scratch {
         return a.val == b.val && isSymmerticHelper(a.left, b.right) && isSymmerticHelper(a.right, b.left);
     }
 
+
+    public boolean check(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || (u.val != v.val)) {
+                return false;
+            }
+
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
+    }
+
 }
 
 class TreeNode {
