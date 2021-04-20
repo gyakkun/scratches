406bee9aa2b6c2335a2be753eb9841fef23f365b
gyakkun <xiaowufeixia@gmail.com>
2021-04-20 10:41:08 +0800

    new

--- scratch_36.java
+++ scratch_36.java
@@ -28,8 +28,37 @@ class Scratch {
 
     }
 
+    // LC116 Solution O(1) space
+    public Node connectSolution(Node root) {
+        if (root == null) {
+            return root;
+        }
+        // 从根节点开始
+        Node leftmost = root;
+        while (leftmost.left != null) {
+            // 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            Node head = leftmost;
+            while (head != null) {
+                // CONNECTION 1
+                head.left.next = head.right;
+                // CONNECTION 2
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                // 指针向后移动
+                head = head.next;
+            }
+            // 去下一层的最左的节点
+            leftmost = leftmost.left;
+        }
+        return root;
+    }
+
     // LC116, O(n) space
     public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
         Deque<Node> q = new LinkedList<>();
         q.offer(root);
         while (!q.isEmpty()) {
