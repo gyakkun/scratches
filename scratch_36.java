7bdbc76eb0e0b32f0b0b19219743f979c87ca87f
gyakkun <xiaowufeixia@gmail.com>
2021-04-20 20:40:24 +0800

    new

--- scratch_36.java
+++ scratch_36.java
@@ -25,6 +25,21 @@ class Scratch {
 
     }
 
+    // LC122
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int[][] dp = new int[n][2]; // dp[i][j] 表示第i天持有j(0/1)份股票时候的最大利润
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]);
+            dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+        }
+        return dp[n][0];
+    }
+
     // LC118
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>(numRows);
