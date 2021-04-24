a413f2e757897df84d9e585f5d5e32e15be212f0
gyakkun <xiaowufeixia@gmail.com>
2021-04-24 08:31:18 +0800

    new

--- scratch_36.java
+++ scratch_36.java
@@ -14,6 +14,21 @@ class Scratch {
 
     }
 
+    // LC377
+    public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j : nums) {
+                if (j <= i) {
+                    dp[i] += dp[i - j];
+                }
+            }
+        }
+        return dp[target];
+    }
+
     // LC137 躺平 选择哈希
     public int singleNumberLc137(int[] nums) {
         Map<Integer, Integer> m = new HashMap<>();
