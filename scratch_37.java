c1e110924e768386f6c031bc30d1023e46f27057
gyakkun <xiaowufeixia@gmail.com>
2021-04-25 10:50:50 +0800

    new

--- scratch_37.java
+++ scratch_37.java
@@ -30,40 +30,23 @@ class Scratch {
 
     // LC138
     public Node copyRandomList(Node head) {
-        Map<Node, Integer> order = new HashMap<>();
-        order.put(null, -1);
-        Map<Integer, Integer> randomOrder = new HashMap<>();
-        int ctr = 0;
-        Node cur = head;
-        while (cur != null) {
-            order.put(cur, ctr++);
-            cur = cur.next;
+        if(head==null){
+            return head;
         }
-        cur = head;
-        while (cur != null) {
-            randomOrder.put(order.get(cur), order.get(cur.random));
-            cur = cur.next;
+        Map<Node, Node> map = new HashMap<>();
+        Node node = head;
+        while(node != null){
+            Node temp = new Node(node.val);
+            map.put(node, temp);
+            node = node.next;
         }
-        Node copyDummy = new Node(-1);
-        Node copyPrev = copyDummy; // newHead = copyDummy.next
-        cur = head;
-        Map<Node, Integer> copyOrder = new HashMap<>();
-        copyOrder.put(null, -1);
-        Map<Integer, Node> copyOrderReverseLookup = new HashMap<>();
-        copyOrderReverseLookup.put(-1, null);
-        while (cur != null) {
-            copyPrev.next = new Node(cur.val);
-            copyPrev = copyPrev.next;
-            copyOrder.put(copyPrev, order.get(cur));
-            copyOrderReverseLookup.put(order.get(cur), copyPrev);
-            cur = cur.next;
+        node = head;
+        while(node!=null){
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
         }
-        Node copyCur = copyDummy.next;
-        while (copyCur != null) {
-            copyCur.random = copyOrderReverseLookup.get(randomOrder.get(copyOrder.get(copyCur)));
-            copyCur = copyCur.next;
-        }
-        return copyDummy.next;
+        return map.get(head);
     }
 
 }
