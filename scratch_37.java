7eb55a986c54e4ec91a1ae203773882519ad5b3c
gyakkun <xiaowufeixia@gmail.com>
2021-04-27 21:21:43 +0800

    new

--- scratch_37.java
+++ scratch_37.java
@@ -1,6 +1,9 @@
 import javafx.util.Pair;
 
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 class Scratch {
     public static void main(String[] args) {
@@ -9,34 +12,134 @@ class Scratch {
 
 //        System.err.println(s.findWords(new char[][]{{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}}, new String[]{"oath", "pea", "eat", "rain"}));
         System.err.println(s.findWords(new char[][]{{'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}}
-
                 , new String[]{"lllllll", "fffffff", "ssss", "s", "rr", "xxxx", "ttt", "eee", "ppppppp", "iiiiiiiii", "xxxxxxxxxx", "pppppp", "xxxxxx", "yy", "jj", "ccc", "zzz", "ffffffff", "r", "mmmmmmmmm", "tttttttt", "mm", "ttttt", "qqqqqqqqqq", "z", "aaaaaaaa", "nnnnnnnnn", "v", "g", "ddddddd", "eeeeeeeee", "aaaaaaa", "ee", "n", "kkkkkkkkk", "ff", "qq", "vvvvv", "kkkk", "e", "nnn", "ooo", "kkkkk", "o", "ooooooo", "jjj", "lll", "ssssssss", "mmmm", "qqqqq", "gggggg", "rrrrrrrrrr", "iiii", "bbbbbbbbb", "aaaaaa", "hhhh", "qqq", "zzzzzzzzz", "xxxxxxxxx", "ww", "iiiiiii", "pp", "vvvvvvvvvv", "eeeee", "nnnnnnn", "nnnnnn", "nn", "nnnnnnnn", "wwwwwwww", "vvvvvvvv", "fffffffff", "aaa", "p", "ddd", "ppppppppp", "fffff", "aaaaaaaaa", "oooooooo", "jjjj", "xxx", "zz", "hhhhh", "uuuuu", "f", "ddddddddd", "zzzzzz", "cccccc", "kkkkkk", "bbbbbbbb", "hhhhhhhhhh", "uuuuuuu", "cccccccccc", "jjjjj", "gg", "ppp", "ccccccccc", "rrrrrr", "c", "cccccccc", "yyyyy", "uuuu", "jjjjjjjj", "bb", "hhh", "l", "u", "yyyyyy", "vvv", "mmm", "ffffff", "eeeeeee", "qqqqqqq", "zzzzzzzzzz", "ggg", "zzzzzzz", "dddddddddd", "jjjjjjj", "bbbbb", "ttttttt", "dddddddd", "wwwwwww", "vvvvvv", "iii", "ttttttttt", "ggggggg", "xx", "oooooo", "cc", "rrrr", "qqqq", "sssssss", "oooo", "lllllllll", "ii", "tttttttttt", "uuuuuu", "kkkkkkkk", "wwwwwwwwww", "pppppppppp", "uuuuuuuu", "yyyyyyy", "cccc", "ggggg", "ddddd", "llllllllll", "tttt", "pppppppp", "rrrrrrr", "nnnn", "x", "yyy", "iiiiiiiiii", "iiiiii", "llll", "nnnnnnnnnn", "aaaaaaaaaa", "eeeeeeeeee", "m", "uuu", "rrrrrrrr", "h", "b", "vvvvvvv", "ll", "vv", "mmmmmmm", "zzzzz", "uu", "ccccccc", "xxxxxxx", "ss", "eeeeeeee", "llllllll", "eeee", "y", "ppppp", "qqqqqq", "mmmmmm", "gggg", "yyyyyyyyy", "jjjjjj", "rrrrr", "a", "bbbb", "ssssss", "sss", "ooooo", "ffffffffff", "kkk", "xxxxxxxx", "wwwwwwwww", "w", "iiiiiiii", "ffff", "dddddd", "bbbbbb", "uuuuuuuuu", "kkkkkkk", "gggggggggg", "qqqqqqqq", "vvvvvvvvv", "bbbbbbbbbb", "nnnnn", "tt", "wwww", "iiiii", "hhhhhhh", "zzzzzzzz", "ssssssssss", "j", "fff", "bbbbbbb", "aaaa", "mmmmmmmmmm", "jjjjjjjjjj", "sssss", "yyyyyyyy", "hh", "q", "rrrrrrrrr", "mmmmmmmm", "wwwww", "www", "rrr", "lllll", "uuuuuuuuuu", "oo", "jjjjjjjjj", "dddd", "pppp", "hhhhhhhhh", "kk", "gggggggg", "xxxxx", "vvvv", "d", "qqqqqqqqq", "dd", "ggggggggg", "t", "yyyy", "bbb", "yyyyyyyyyy", "tttttt", "ccccc", "aa", "eeeeee", "llllll", "kkkkkkkkkk", "sssssssss", "i", "hhhhhh", "oooooooooo", "wwwwww", "ooooooooo", "zzzz", "k", "hhhhhhhh", "aaaaa", "mmmmm"}));
-
         // words = {"oath","pea","eat","rain"}
 
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
     }
 
-    // LC212 Hard
-    Set<String> lc212Result = new HashSet<>();
+    // LC212 Hard, 多线程版本效果不好
+    ConcurrentMap<String, Integer> lc212Result;
+    HashSet<String> lc212ResultStr;
+    int boardRow;
+    int boardCol;
 
     public List<String> findWords(char[][] board, String[] words) {
         Trie trie = new Trie();
         for (String word : words) {
             trie.insert(word);
         }
-        boolean[][] visited;
+        lc212Result = new ConcurrentHashMap<>();
+        boardRow = board.length;
+        boardCol = board[0].length;
+        ForkJoinPool fjp = new ForkJoinPool();
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                visited = new boolean[board.length][board[0].length];
-                visited[i][j] = true;
-                lc79Backtrack(board, trie, i, j, "" + board[i][j], visited, -1);
+                lc79 job = new lc79(board, trie, i, j, "" + board[i][j], new boolean[boardRow][boardCol], -1);
+                fjp.submit(job);
             }
         }
-        return new LinkedList<>(lc212Result);
+        while(fjp.getActiveThreadCount()!=0){
+            ;
+        }
+        return new LinkedList<>(lc212Result.keySet());
     }
 
+    public class lc79 extends RecursiveAction {
+
+        char[][] board;
+        Trie trie;
+        int curRow;
+        int curCol;
+        String curWord;
+        boolean[][] visited;
+        int direct;
+        ReentrantReadWriteLock rwl;
+        Lock rLVisited;
+        Lock wLVisited;
+
+        public lc79(char[][] board, Trie trie, int curRow, int curCol, String curWord, boolean[][] visited, int direct) {
+            this.board = board;
+            this.trie = trie;
+            this.curRow = curRow;
+            this.curCol = curCol;
+            this.curWord = curWord;
+            this.visited = visited;
+            this.direct = direct;
+        }
+
+        public lc79(char[][] board, Trie trie, int curRow, int curCol, String curWord, boolean[][] visited, int direct, Lock rLVisited, Lock wLVisited) {
+            this.board = board;
+            this.trie = trie;
+            this.curRow = curRow;
+            this.curCol = curCol;
+            this.curWord = curWord;
+            this.visited = visited;
+            this.direct = direct;
+            this.rLVisited = rLVisited;
+            this.wLVisited = wLVisited;
+        }
+
+        @Override
+        protected void compute() {
+            if (direct == -1) {
+                this.rwl = new ReentrantReadWriteLock();
+                this.rLVisited = rwl.readLock();
+                this.wLVisited = rwl.writeLock();
+                try {
+                    wLVisited.lock();
+                    visited[curRow][curCol] = true;
+                } finally {
+                    wLVisited.unlock();
+                }
+            }
+            if (!trie.startsWith(curWord)) {
+                return;
+            }
+            if (trie.search(curWord)) {
+                lc212Result.putIfAbsent(curWord, 1);
+            }
+
+            int[][] options = new int[][]{{curRow - 1, curCol, 1, 0}, {curRow + 1, curCol, 0, 1}, {curRow, curCol - 1, 3, 2}, {curRow, curCol + 1, 2, 3}};
+            for (int[] option : options) {
+                if (checkLegalPosition(option[0], option[1]) && direct != option[2]) {
+                    try {
+                        wLVisited.lock();
+                        visited[option[0]][option[1]] = true;
+                    } finally {
+                        wLVisited.unlock();
+                    }
+
+                    lc79 job = new lc79(board, trie, option[0], option[1], curWord + board[option[0]][option[1]], visited, option[3], this.rLVisited, this.wLVisited);
+                    job.fork();
+
+                    try {
+                        wLVisited.lock();
+                        visited[option[0]][option[1]] = false;
+                    } finally {
+                        wLVisited.unlock();
+                    }
+                }
+            }
+
+            return;
+        }
+
+        private boolean checkLegalPosition(int row, int col) {
+            boolean flag1 = (row >= 0 && row < boardRow && col >= 0 && col < boardCol);
+            if (!flag1) return false;
+            boolean flag2 = false;
+            try {
+                rLVisited.lock();
+                flag2 = visited[row][col];
+            } finally {
+                rLVisited.unlock();
+            }
+            return flag1 && !flag2;
+        }
+    }
 
     private void lc79Backtrack(char[][] board, Trie trie, int curRow, int curCol,
                                String curWord, boolean[][] visited, int direct) { // 0123 - 上下左右
@@ -44,7 +147,7 @@ class Scratch {
             return;
         }
         if (trie.search(curWord)) {
-            lc212Result.add(curWord);
+            lc212ResultStr.add(curWord);
         }
         StringBuffer sb = new StringBuffer(curWord);
         // 上下左右
