b390ceffe30830c7c81cfd97d6bc8944729e6b33
gyakkun <xiaowufeixia@gmail.com>
2021-04-28 16:01:26 +0800

    new

--- scratch_39.java
+++ scratch_39.java
@@ -1,14 +1,33 @@
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 class Scratch {
     public static void main(String[] args) {
         Scratch s = new Scratch();
         long timing = System.currentTimeMillis();
-        System.err.println(s.containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+        System.err.println(s.containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
 
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC230
+    int lc230Ctr = 0;
+    int lc230Result = -1;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return lc230Result;
+    }
+
+    private void inorder(TreeNode root, int k) {
+        if (root.left != null) inorder(root.left, k);
+        lc230Ctr++;
+        if (lc230Ctr == k) {
+            lc230Result = root.val;
+        }
+        if (root.right != null) inorder(root.right, k);
     }
 
     // LC633
@@ -29,5 +48,30 @@ class Scratch {
             if (!s.add(i)) return true;
         }
         return false;
+    }
+
+    // LC218 TBD
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        return null;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
