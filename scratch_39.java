cf031bf28c7b89ec0d9fea5e2ef0193545383924
gyakkun <xiaowufeixia@gmail.com>
2021-04-28 09:57:52 +0800

    new

--- scratch_39.java
+++ scratch_39.java
@@ -1,8 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Scratch {
     public static void main(String[] args) {
         Scratch s = new Scratch();
         long timing = System.currentTimeMillis();
-        System.err.println(s.judgeSquareSum(2147483646));
+        System.err.println(s.containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
 
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
@@ -15,6 +18,15 @@ class Scratch {
             if (b == (int) b) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    // LC217
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for (int i : nums) {
+            if (!s.add(i)) return true;
         }
         return false;
     }
