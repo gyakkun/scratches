778119fd23b13d5d5a2cacbe3b4672390516771a
gyakkun <xiaowufeixia@gmail.com>
2021-05-07 16:27:51 +0800

    new

--- scratch_39.java
+++ scratch_39.java
@@ -14,6 +14,25 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC454
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int result = 0;
+        Map<Integer, Integer> map12 = new HashMap<>();
+        for (int a : nums1) {
+            for (int b : nums2) {
+                map12.put(a + b, map12.getOrDefault(a + b, 0) + 1);
+            }
+        }
+        for (int c : nums3) {
+            for (int d : nums4) {
+                if (map12.containsKey(-c - d)) {
+                    result += map12.get(-c - d);
+                }
+            }
+        }
+        return result;
+    }
+
     // LC412
     public List<String> fizzBuzz(int n) {
         List<String> l = new ArrayList<>(n);
