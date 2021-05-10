68aef068492965d41225208d09d4a771b6455ea1
gyakkun <xiaowufeixia@gmail.com>
2021-05-10 15:24:51 +0800

    new

--- scratch_40.java
+++ scratch_40.java
@@ -4,17 +4,22 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class Scratch {
-    public static void main(String[] args) throws IOException {
+    public static void main1(String[] args) throws IOException {
         System.out.println(maxGcRatio("AAAACCACCCCACAAAAA", 5));
     }
 
-    public static void main2(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 //        System.out.println(learnEnglish(969150, false));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String n;
-        while ((n = br.readLine()) != null) {
-            int minLen = Integer.valueOf(br.readLine());
-            System.out.println(maxGcRatio(n, minLen));
+
+        n = br.readLine();
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.calculate(n));
+
+//        while ((n = br.readLine()) != null) {
+//            int minLen = Integer.valueOf(br.readLine());
+//            System.out.println(maxGcRatio(n, minLen));
 
 //            String ip1S = br.readLine().trim();
 //            String ip2S = br.readLine().trim();
@@ -23,8 +28,9 @@ class Scratch {
 //            Integer[] ip2 = Arrays.stream(ip2S.trim().split(" ")).map(Integer::valueOf).toArray(Integer[]::new);
 //            int num = numOfWeights(n, ip1, ip2);
 //            System.out.println(num);
-        }
+//        }
     }
+
 
     // HJ63 算最高GC比例的子串, 定义的应该是最小子串长度而不是固定子串长度
     public static String maxGcRatio(String gene, int minLen) {
@@ -42,7 +48,7 @@ class Scratch {
             for (int j = 0; j <= (gene.length() - i); j++) {
                 int count = prefix[j + i] - prefix[j];
                 double ratio = (double) count / (double) i;
-                if(ratio>maxRatio){
+                if (ratio > maxRatio) {
                     maxRatio = ratio;
                     startIdx = j;
                     maxLen = i;
@@ -153,4 +159,120 @@ class Scratch {
 //    }
 }
 
+// HJ54
+class Calculator {
+    public int calculate(String s) {
+        return (int) evalRPN(toRPN(decodeExpression(s)));
+    }
 
+    public List<String> toRPN(List<String> express) {
+        List<String> rpn = new LinkedList<>();
+        Deque<String> stack = new LinkedList<>();
+        Set<String> notNumber = new HashSet<String>() {{
+            add("+");
+            add("-");
+            add("/");
+            add("*");
+            add("(");
+            add(")");
+        }};
+        String tmp;
+        for (String token : express) {
+            if (!notNumber.contains(token)) {
+                rpn.add(token);
+            } else if (token.equals("(")) {
+                stack.push(token);
+            } else if (token.equals(")")) {
+                while (!(tmp = stack.pop()).equals("(")) {
+                    rpn.add(tmp);
+                }
+
+            } else {
+                while (!stack.isEmpty() && getOperPriority(stack.peek()) >= getOperPriority(token)) {
+                    rpn.add(stack.pop());
+                }
+                stack.push(token);
+            }
+        }
+        while (!stack.isEmpty()) {
+            rpn.add(stack.pop());
+        }
+        return rpn;
+    }
+
+    private List<String> decodeExpression(String express) {
+        express = express.replaceAll("\\ ", "");
+        express = express.replaceAll("\\(\\+", "(0+");
+        express = express.replaceAll("\\(\\-", "(0-");
+        express = express.replaceAll("\\((\\d+\\.?\\d*)\\)", "$1");
+        List<String> result = new LinkedList<>();
+        int i = 0;
+        StringBuffer sb;
+        do {
+            if ((express.charAt(i) < '0' || express.charAt(i) > '9') && express.charAt(i) != '.') {
+                result.add(express.charAt(i) + "");
+                i++;
+            } else {
+                sb = new StringBuffer();
+                while (i < express.length() && ((express.charAt(i) >= '0' && express.charAt(i) <= '9') || express.charAt(i) == '.')) {
+                    sb.append(express.charAt(i));
+                    i++;
+                }
+                result.add(sb.toString());
+            }
+        } while (i < express.length());
+        return result;
+    }
+
+    private int getOperPriority(String oper) {
+        switch (oper) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                return -1;
+        }
+    }
+
+    // LC150 逆波兰表达式
+    public int evalRPN(List<String> tokens) {
+        Deque<String> stack = new LinkedList<>();
+        stack.push("0");
+        Set<String> oper = new HashSet<String>() {{
+            add("+");
+            add("-");
+            add("/");
+            add("*");
+        }};
+        for (String token : tokens) {
+            if (oper.contains(token)) {
+                int a = Integer.parseInt(stack.pop());
+                int b = Integer.parseInt(stack.pop());
+                int tmp;
+                switch (token) {
+                    case "+":
+                        tmp = a + b;
+                        break;
+                    case "-":
+                        tmp = b - a;
+                        break;
+                    case "/":
+                        tmp = b / a;
+                        break;
+                    case "*":
+                        tmp = a * b;
+                        break;
+                    default:
+                        tmp = 0;
+                }
+                stack.push(String.valueOf(tmp));
+            } else {
+                stack.push(token);
+            }
+        }
+        return stack.isEmpty() ? 0 : Integer.parseInt(stack.pop());
+    }
+}
