5f11b5b7d5966ff9b81f6fb69b3c6b13a0ad06d3
gyakkun <xiaowufeixia@gmail.com>
2021-05-13 10:21:28 +0800

    new

--- scratch_41.java
+++ scratch_41.java
@@ -1,24 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 class Scratch {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String n;
-        n = br.readLine();
-        int numOfPeople = Integer.valueOf(n);
-        int[][] money = new int[numOfPeople][numOfPeople];
-        for (int i = 0; i < numOfPeople; i++) {
-            n = br.readLine();
-            String[] a = n.split(" ");
-            money[i][0] = Integer.valueOf(a[0]);
-            money[i][1] = Integer.valueOf(a[1]);
+        while((n = br.readLine())!=null) {
+            int i = Integer.valueOf(n);
+            System.out.println(numOfPerfectNum(i));
         }
-        System.out.println(primeoj1002(money));
+    }
 
+    // HJ56
+    static Map<Integer, Set<Integer>> numFactorMap = new HashMap<>();
+    static List<Integer> perfectNum = new LinkedList<>();
+    static int hj56Result = 0;
+    static int[] hj56Table = new int[]{0, 8128, 496, 28, 6};
+
+    public static int numOfPerfectNumTable(int n) {
+        for (int i = 1; i <= 4; i++) {
+            if (n >= hj56Table[i]) {
+                return 11 - i + 1;
+            }
+        }
+        return 0;
+    }
+
+    public static int numOfPerfectNum(int n) {
+        for (int i = 6; i <= n; i++) {
+            calFactor(i);
+        }
+        Collections.reverse(perfectNum);
+        System.out.println(perfectNum);
+        return hj56Result;
+    }
+
+    private static void calFactor(int n) {
+        int upperBound = (int) (Math.sqrt(n) + 1);
+        Set<Integer> s = new HashSet<>();
+        boolean[] visited = new boolean[upperBound + 1];
+        for (int i = 1; i <= upperBound; i++) {
+            if (!visited[i] && n % i == 0) {
+                s.add(i);
+                s.add(n / i);
+                if (numFactorMap.containsKey(i)) {
+                    Set<Integer> tmp = numFactorMap.get(i);
+                    Iterator<Integer> it = tmp.iterator();
+                    while (it.hasNext()) {
+                        int j = it.next();
+                        s.add(j);
+                        visited[j] = true;
+                    }
+                }
+//                if (numFactorMap.containsKey(n / i)) {
+//                    Set<Integer> tmp = numFactorMap.get(n / i);
+//                    Iterator<Integer> it = tmp.iterator();
+//                    while (it.hasNext()) {
+//                        int j = it.next();
+//                        s.add(j);
+//                        if (j <= upperBound) {
+//                            visited[j] = true;
+//                        }
+//                    }
+//                }
+                visited[i] = true;
+            }
+        }
+        numFactorMap.put(n, s);
+        Iterator<Integer> it = s.iterator();
+        int sumFactor = 0;
+        while (it.hasNext()) {
+            int i = it.next();
+            if (i != n) sumFactor += i;
+        }
+        if (sumFactor == n) {
+            hj56Result++;
+            perfectNum.add(n);
+        }
     }
 
     // PRIMEOJ 1002
