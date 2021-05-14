13983d9a885bad0c0430be45b42cf73964923889
gyakkun <xiaowufeixia@gmail.com>
2021-05-14 16:22:19 +0800

    new

--- scratch_42.java
+++ scratch_42.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 class Scratch {
@@ -8,10 +10,38 @@ class Scratch {
         int[] arr = new int[]{1, 4, 2, 3};
         int k = 4;
         System.err.println(
-                s.medianSlidingWindow(arr, k)
+                s.letterCasePermutation("12345")
         );
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC784
+    public List<String> letterCasePermutation(String S) {
+        List<String> result = new ArrayList<>();
+        List<Integer> letterIdx = new ArrayList<>();
+        for (int i = 0; i < S.length(); i++) {
+            if (Character.isLetter(S.charAt(i))) {
+                letterIdx.add(i);
+            }
+        }
+        char[] lowerCase = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] upperCase = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        int maxMask = 1 << letterIdx.size();
+        for (int mask = 0; mask < maxMask; mask++) {
+            StringBuffer sb = new StringBuffer(S);
+            for (int i = 0; i < letterIdx.size(); i++) {
+                // 1 变 0 不变
+                if (((mask >> i) & 1) == 1) {
+                    int idx = letterIdx.get(i);
+                    char c = S.charAt(idx);
+                    sb.setCharAt(idx, Character.isLowerCase(c) ? upperCase[c - 'a'] : lowerCase[c - 'A']);
+                }
+            }
+            result.add(sb.toString());
+        }
+
+        return result;
     }
 
     // LC480
