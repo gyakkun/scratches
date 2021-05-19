dfde2ff2169467f9c59385e9d5aeaa65f54434c7
gyakkun <xiaowufeixia@gmail.com>
2021-05-19 15:12:52 +0800

    new

--- scratch_42.java
+++ scratch_42.java
@@ -652,3 +652,34 @@ class TreeNode {
         this.right = right;
     }
 }
+
+// LC155
+class MinStack {
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        minStack.push(Math.min(minStack.peek(), val));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
