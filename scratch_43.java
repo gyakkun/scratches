9e4f007df52a715ff7ee47cb59bfacf89894d4a6
gyakkun <xiaowufeixia@gmail.com>
2021-05-27 14:03:22 +0800

    new

---
+++ scratch_43.java
@@ -0,0 +1,49 @@
+import javafx.util.*;
+
+import java.util.*;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scratch s = new Scratch();
+        long timing = System.currentTimeMillis();
+        int[] arr = new int[]{4,-1,4,-2,4};
+        int[][] ob = new int[][]{{2, 4}};
+        System.err.println(s.robotSim(arr, ob));
+        timing = System.currentTimeMillis() - timing;
+        System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC874
+    public int robotSim(int[] commands, int[][] obstacles) {
+        // -2 左转90度
+        // -1 右转90度
+        int x = 0, y = 0;
+        int direct = 0; // 0 - 北, 1 - 东 , 2 - 南, 3 - 西
+        int[][] step = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int ans = 0;
+        Set<Pair<Integer, Integer>> obSet = new HashSet<>();
+        for (int[] i : obstacles) {
+            obSet.add(new Pair<>(i[0], i[1]));
+        }
+        for (int c : commands) {
+            if (c < 0) {
+                if (c == -2) {
+                    direct = (direct + 4 - 1) % 4;
+                } else if (c == -1) {
+                    direct = (direct + 1) % 4;
+                }
+            } else {
+                for (int j = 0; j < c; j++) {
+                    if (!obSet.contains(new Pair<>(x + step[direct][0], y + step[direct][1]))) {
+                        x += step[direct][0];
+                        y += step[direct][1];
+                        ans = Math.max(ans, x * x + y * y);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
