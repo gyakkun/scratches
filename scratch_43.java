dbbedaac128ff18ea275183c0eb01290baf49ef6
gyakkun <xiaowufeixia@gmail.com>
2021-06-03 17:31:58 +0800

    new

--- scratch_43.java
+++ scratch_43.java
@@ -7,22 +7,22 @@ class Scratch {
         Scratch s = new Scratch();
         long timing = System.currentTimeMillis();
 
-        System.err.println(s.smallestSufficientTeam(new String[]{"java", "nodejs", "reactjs"}, new String[][]{{"java"}, {"nodejs"}, {"nodejs", "reactjs"}}));
+        System.err.println("");
 
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
     }
 
     // LC1125 状压DP 0/1背包
-    public int[] smallestSufficientTeam(String[] reqSkills, String[][] people) {
+    public int[] smallestSufficientTeam(String[] reqSkills, List<List<String>> people) {
         Map<String, Integer> skillIdxMap = new HashMap<>();
         for (int i = 0; i < reqSkills.length; i++) {
             skillIdxMap.put(reqSkills[i], i);
         }
         int maxMask = 1 << reqSkills.length;
-        int[] peopleSkillMask = new int[people.length];
-        for (int i = 0; i < people.length; i++) {
-            for (String skill : people[i]) {
+        int[] peopleSkillMask = new int[people.size()];
+        for (int i = 0; i < people.size(); i++) {
+            for (String skill : people.get(i)) {
                 if (skillIdxMap.containsKey(skill)) {
                     peopleSkillMask[i] |= 1 << skillIdxMap.get(skill);
                 }
@@ -30,16 +30,16 @@ class Scratch {
         }
 
         // dp: mask - 表示当前状态是否可达(存在即可达), value里面存的是人的下标
-        Map<Integer, Set<Integer>> dp = new HashMap<>();
-        dp.put(0, new HashSet<>());
+        Map<Integer, List<Integer>> dp = new HashMap<>();
+        dp.put(0, new ArrayList<>());
         for (int i = 0; i < maxMask; i++) {
             if (dp.containsKey(i)) {
-                for (int j = 0; j < people.length; j++) {
+                for (int j = 0; j < people.size(); j++) {
                     if (peopleSkillMask[j] == 0) continue;
                     // 加入这个人的技能之后的状态
                     int afterJoin = i | peopleSkillMask[j];
                     if (!dp.containsKey(afterJoin) || dp.get(afterJoin).size() > dp.get(i).size() + 1) {
-                        dp.put(afterJoin, new HashSet(dp.get(i))); // 必须复制一份Set, 不然直接改引用, 所有的dp[i]都共用同一个set
+                        dp.put(afterJoin, new ArrayList<>(dp.get(i))); // 必须复制一份Set, 不然直接改引用, 所有的dp[i]都共用同一个set
                         dp.get(afterJoin).add(j);
                     }
                 }
