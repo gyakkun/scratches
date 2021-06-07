a63d0c9c93f0d07640740ca83d0115d34287f066
gyakkun <xiaowufeixia@gmail.com>
2021-06-07 11:17:18 +0800

    new

--- scratch_44.java
+++ scratch_44.java
@@ -276,3 +276,40 @@ class ZeroEvenOdd {
         }
     }
 }
+
+// LC1115 多线程
+class FooBar {
+    private int n;
+    volatile boolean isFoo;
+
+    public FooBar(int n) {
+        this.n = n;
+        isFoo = true;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (!isFoo)
+                    this.wait();
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                this.isFoo = false;
+                this.notifyAll();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (isFoo)
+                    this.wait();
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                this.isFoo = true;
+                this.notifyAll();
+            }
+        }
+    }
+}
