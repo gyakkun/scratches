640014493e6d88ab13ac0afbb904125ee9fafa62
gyakkun <xiaowufeixia@gmail.com>
2021-06-10 10:21:27 +0800

    new

--- scratch_44.java
+++ scratch_44.java
@@ -19,6 +19,19 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC322
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE / 2 ? -1 : dp[amount];
+    }
+
     // LC518 Learn from Solution
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
