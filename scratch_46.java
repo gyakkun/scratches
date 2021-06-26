d6685162a5dc71f36fa2b23c6d6f84b55cb4460f
gyakkun <xiaowufeixia@gmail.com>
2021-06-26 15:25:54 +0800

    new

--- scratch_46.java
+++ scratch_46.java
@@ -15,6 +15,33 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+
+    // LC402 Solution **
+    public String removeKdigits(String num, int k) {
+        Deque<Character> deque = new LinkedList<>();
+        int length = num.length();
+        for (int i = 0; i < length; ++i) {
+            char digit = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peek() > digit) {
+                deque.pop();
+                k--;
+            }
+            deque.push(digit);
+        }
+        for (int i = 0; i < k; ++i) {
+            deque.pop();
+        }
+
+        StringBuilder ret = new StringBuilder();
+        while (!deque.isEmpty() && deque.peekLast() == '0') {
+            deque.pollLast();
+        }
+        while (!deque.isEmpty()) {
+            ret.append(deque.pollLast());
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
+    }
+
     // LC773
     public int slidingPuzzle(int[][] board) {
         boolean[][][][][] visited = new boolean[6][6][6][6][6]; // 第六个格子可以由前5个推出
@@ -62,36 +89,6 @@ class Scratch {
             }
         }
         return -1;
-    }
-
-    // LC402 Solution ** TBD
-    public String removeKdigits(String num, int k) {
-        Deque<Character> deque = new LinkedList<Character>();
-        int length = num.length();
-        for (int i = 0; i < length; ++i) {
-            char digit = num.charAt(i);
-            while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
-                deque.pollLast();
-                k--;
-            }
-            deque.offerLast(digit);
-        }
-
-        for (int i = 0; i < k; ++i) {
-            deque.pollLast();
-        }
-
-        StringBuilder ret = new StringBuilder();
-        boolean leadingZero = true;
-        while (!deque.isEmpty()) {
-            char digit = deque.pollFirst();
-            if (leadingZero && digit == '0') {
-                continue;
-            }
-            leadingZero = false;
-            ret.append(digit);
-        }
-        return ret.length() == 0 ? "0" : ret.toString();
     }
 
     // LC1342
