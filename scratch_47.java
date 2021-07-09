42c7569777abc1e25dbd8467f29729b03bdf1c78
gyakkun <xiaowufeixia@gmail.com>
2021-07-09 09:45:20 +0800

    new

--- scratch_47.java
+++ scratch_47.java
@@ -222,3 +222,38 @@ class LFUCache {
         }
     }
 }
+
+
+// LC225
+class MyStack {
+    Queue<Integer> q;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        int size = q.size();
+        q.offer(x);
+        for (int i = 0; i < size; i++) {
+            q.offer(q.poll());
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return q.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
