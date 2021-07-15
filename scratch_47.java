aba10e542b504026d172c509bcf795411571524e
gyakkun <xiaowufeixia@gmail.com>
2021-07-15 22:56:32 +0800

    new

--- scratch_47.java
+++ scratch_47.java
@@ -14,6 +14,18 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC120 Bottom Up
+    public int minimumTotalBottomUp(List<List<Integer>> triangle) {
+        int[][] dp = new int[2][triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i % 2][j] = Math.min(dp[(i + 1) % 2][j], dp[(i + 1) % 2][j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
+    }
+
+
     // LC120 Top Down
     Integer[][] lc120Memo;
 
