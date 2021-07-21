4d94107598529dd6e17665db66c1b60371f3644a
gyakkun <xiaowufeixia@gmail.com>
2021-07-21 09:50:51 +0800

    new

--- scratch_48.java
+++ scratch_48.java
@@ -8,6 +8,28 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) return head;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode lastSorted = head, cur = head.next;
+        while (cur != null) {
+            if (lastSorted.val <= cur.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummy;
+                while (prev.next.val <= cur.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = cur.next;
+                cur.next = prev.next;
+                prev.next = cur;
+            }
+            cur = lastSorted.next;
+        }
+        return dummy.next;
+    }
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) return null;
         ListNode aPtr = headA, bPtr = headB;
