651ad81ac14e042a7e9968873e36392be573986c
gyakkun <xiaowufeixia@gmail.com>
2021-07-28 14:37:39 +0800

    new

--- scratch_48.java
+++ scratch_48.java
@@ -14,6 +14,21 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC704
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     // LCP28
     public int purchasePlans(int[] nums, int target) {
         int n = nums.length;
