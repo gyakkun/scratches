572029f4fc6d47c5694f519f2c438485a13b1129
gyakkun <xiaowufeixia@gmail.com>
2021-07-30 13:49:28 +0800

    new

--- scratch_48.java
+++ scratch_48.java
@@ -7,16 +7,51 @@ class Scratch {
         long timing = System.currentTimeMillis();
 
 
-        System.out.println(s.findCheapestPrice(
-                13,
-                new int[][]{{11, 12, 74}, {1, 8, 91}, {4, 6, 13}, {7, 6, 39}, {5, 12, 8}, {0, 12, 54}, {8, 4, 32}, {0, 11, 4}, {4, 0, 91}, {11, 7, 64}, {6, 3, 88}, {8, 5, 80}, {11, 10, 91}, {10, 0, 60}, {8, 7, 92}, {12, 6, 78}, {6, 2, 8}, {4, 3, 54}, {3, 11, 76}, {3, 12, 23}, {11, 6, 79}, {6, 12, 36}, {2, 11, 100}, {2, 5, 49}, {7, 0, 17}, {5, 8, 95}, {3, 9, 98}, {8, 10, 61}, {2, 12, 38}, {5, 7, 58}, {9, 4, 37}, {8, 6, 79}, {9, 0, 1}, {2, 3, 12}, {7, 10, 7}, {12, 10, 52}, {7, 2, 68}, {12, 2, 100}, {6, 9, 53}, {7, 4, 90}, {0, 5, 43}, {11, 2, 52}, {11, 8, 50}, {12, 4, 38}, {7, 9, 94}, {2, 7, 38}, {3, 7, 88}, {9, 12, 20}, {12, 0, 26}, {10, 5, 38}, {12, 8, 50}, {0, 2, 77}, {11, 0, 13}, {9, 10, 76}, {2, 6, 67}, {5, 6, 34}, {9, 7, 62}, {5, 3, 67}},
-                10,
-                1,
-                10
-        ));
+        System.out.println(s.jump(new int[]{2, 3, 0, 1, 4}));
 
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC45 MLE Dijkstra
+    public int jump(int[] nums) {
+        if (nums.length == 1) return 0;
+        int n = nums.length;
+        int[][] matrix = new int[n][n];
+        for (int[] i : matrix) Arrays.fill(i, -1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= nums[i]; j++) {
+                if (i + j < n) {
+                    matrix[i][i + j] = 1;
+                }
+                if (i - j >= 0) {
+                    matrix[i][i - j] = 1;
+                }
+            }
+        }
+
+        // Dijkstra?
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0);
+        for (int i = 1; i < n; i++) {
+            int curMinIdx = 0;
+            int curMinLen = Integer.MAX_VALUE;
+            for (int j = 1; j < n; j++) {
+                if (!visited.contains(j) && matrix[0][j] != -1 && curMinLen > matrix[0][j]) {
+                    curMinIdx = j;
+                    curMinLen = matrix[0][j];
+                }
+            }
+            visited.add(curMinIdx);
+            for (int j = 1; j < n; j++) {
+                if (!visited.contains(j)) {
+                    if (matrix[curMinIdx][j] != -1 && (matrix[0][j] == -1 || matrix[0][j] > matrix[0][curMinIdx] + matrix[curMinIdx][j])) {
+                        matrix[0][j] = matrix[0][curMinIdx] + matrix[curMinIdx][j];
+                    }
+                }
+            }
+        }
+        return matrix[0][n - 1];
     }
 
     // LC787 DFS 不存在环 TLE
