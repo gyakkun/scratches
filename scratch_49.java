c69cdb8b8bd364593a4a4caf7796850d7edfd766
gyakkun <xiaowufeixia@gmail.com>
2021-08-04 15:10:39 +0800

    new

--- scratch_49.java
+++ scratch_49.java
@@ -358,3 +358,28 @@ class Solution {
         return new double[]{x, y};
     }
 }
+
+// JZOF 59
+class KthLargest {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for (int i : nums) {
+            add(i);
+        }
+    }
+
+    public int add(int val) {
+        if (pq.size() < k) {
+            pq.offer(val);
+        } else {
+            if (val > pq.peek()) {
+                pq.poll();
+                pq.offer(val);
+            }
+        }
+        return pq.peek();
+    }
+}
