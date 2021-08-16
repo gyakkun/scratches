d105f5340f1e2ce465263a0e52bf4ddd953df88e
gyakkun <xiaowufeixia@gmail.com>
2021-08-16 16:20:40 +0800

    new

--- scratch_50.java
+++ scratch_50.java
@@ -13,7 +13,29 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
-    // JZOF 68 II
+    // JZOF II 090
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[1], nums[0]);
+        // ROB ZERO, then n-1 can't be robbed
+        dp[0] = dp[1] = nums[0];
+        for (int i = 2; i < n - 1; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        int noZero = dp[n - 2];
+        Arrays.fill(dp, 0);
+        // Not rob zero, then n-1 can be robbed
+        dp[1] = nums[1];
+        dp[2] = Math.max(nums[1], nums[2]);
+        for (int i = 3; i < n; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        int zero = dp[n - 1];
+        return Math.max(zero, noZero);
+    }
+
     public TreeNode LCA(TreeNode root, TreeNode p, TreeNode q) {
         Map<TreeNode, TreeNode> parent = new HashMap<>();
         parent.put(root, null);
