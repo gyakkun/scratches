cb94a12854839fb14e589ad836e897d821ed97aa
gyakkun <xiaowufeixia@gmail.com>
2021-08-19 20:25:55 +0800

    new

--- scratch_50.java
+++ scratch_50.java
@@ -15,6 +15,20 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC991
+    public int brokenCalc(int startValue, int target) {
+        int result = 0;
+        while (target > startValue) {
+            result++;
+            if (target % 2 == 1) {
+                target++;
+            } else {
+                target /= 2;
+            }
+        }
+        return result + startValue - target;
+    }
+
     // LC1144
     public int movesToMakeZigzag(int[] nums) {
         if (nums.length <= 2) return 0;
