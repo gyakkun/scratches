7c13a291d3ceaa52eeb282e8b3c149645bfa4bce
gyakkun <xiaowufeixia@gmail.com>
2021-08-23 09:34:40 +0800

    new

--- scratch_50.java
+++ scratch_50.java
@@ -9,10 +9,24 @@ class Scratch {
         long timing = System.currentTimeMillis();
 
 //        System.out.println(s.compress(new char[]{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}));
-        System.out.println(s.compress(new char[]{'a', 'a', 'b', 'b', 'c', 'c', 'c'}));
+        System.out.println(s.getMaximumGenerated(100));
 
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC1646
+    public int getMaximumGenerated(int n) {
+        if (n <= 1) return n;
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 1; i * 2 + 1 <= n; i++) {
+            arr[2 * i] = arr[i];
+            arr[2 * i + 1] = arr[i] + arr[i + 1];
+        }
+        if ((n / 2) * 2 == n) arr[n] = arr[n / 2];
+        return Arrays.stream(arr).max().getAsInt();
     }
 
     // LC789 ** 曼哈顿距离及推理见Solution
