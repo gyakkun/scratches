a2f5dbe2e98fade04e58ce3dc15a8cc64d2a3a25
gyakkun <xiaowufeixia@gmail.com>
2021-08-24 16:32:24 +0800

    new

--- scratch_51.java
+++ scratch_51.java
@@ -14,6 +14,23 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // JZOF II 098
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][1] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= m; i++) {
+            for (int j = 2; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m][n];
+    }
+
     // LC1749
     public int maxAbsoluteSum(int[] nums) {
         int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, curSeg = 0;
