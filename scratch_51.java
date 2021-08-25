1f927b64f0ccbfe129fd6790cba048289e890dfe
gyakkun <xiaowufeixia@gmail.com>
2021-08-25 10:55:08 +0800

    new

--- scratch_51.java
+++ scratch_51.java
@@ -14,6 +14,25 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // Interview 01.07
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) { // 先上下 再斜对角线(左上, 右下)
+            for (int j = 0; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = tmp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+
     // LC797
     List<List<Integer>> lc797Result;
 
