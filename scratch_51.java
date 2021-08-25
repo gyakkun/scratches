c7d8758eb03452dbcc1fa20373c1eb3023ab7de0
gyakkun <xiaowufeixia@gmail.com>
2021-08-25 17:10:04 +0800

    new

--- scratch_51.java
+++ scratch_51.java
@@ -14,6 +14,28 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC41 **
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int val = Math.abs(nums[i]);
+            if (val <= n) {
+                if (nums[val - 1] >= 0) {
+                    nums[val - 1] = -nums[val - 1];
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) return i + 1;
+        }
+        return n + 1;
+    }
+
     // LC435 **
     public int eraseOverlapIntervals(int[][] intervals) {
         if (intervals.length <= 1) return intervals.length;
