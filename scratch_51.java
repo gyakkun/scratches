38b6857055eb6a1040fda0045845267f9c301764
gyakkun <xiaowufeixia@gmail.com>
2021-08-31 14:26:16 +0800

    new

--- scratch_51.java
+++ scratch_51.java
@@ -24,6 +24,27 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC335 **
+    public boolean isSelfCrossing(int[] distance) {
+        for (int i = 3; i < distance.length; i++) {
+            if (i >= 3
+                    && distance[i] >= distance[i - 2]
+                    && distance[i - 1] <= distance[i - 3])
+                return true;
+            else if (i >= 4
+                    && distance[i] + distance[i - 4] >= distance[i - 2]
+                    && distance[i - 1] == distance[i - 3])
+                return true;
+            else if (i >= 5
+                    && distance[i] + distance[i - 4] >= distance[i - 2]
+                    && distance[i - 5] + distance[i - 1] >= distance[i - 3]
+                    && distance[i - 2] > distance[i - 4]
+                    && distance[i - 3] > distance[i - 1])
+                return true;
+        }
+        return false;
+    }
+
     // LC755
     public int[] pourWater(int[] heights, int volume, int k) {
         while (volume-- > 0) {
