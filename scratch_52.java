f60943c2c7a19f18b135d3cf3f2f3d81f30f9a85
gyakkun <xiaowufeixia@gmail.com>
2021-09-01 18:40:21 +0800

    new

---
+++ scratch_52.java
@@ -0,0 +1,49 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scratch s = new Scratch();
+        long timing = System.currentTimeMillis();
+
+        System.out.println(s.eatenApples(new int[]{1, 2, 3, 5, 2},
+                new int[]{3, 2, 1, 4, 2}));
+
+        timing = System.currentTimeMillis() - timing;
+        System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC1705
+    public int eatenApples(int[] apples, int[] days) {
+        // pq 存数对 [i,j], i表示苹果数量, j表示过期时间
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        int n = apples.length;
+        int result = 0;
+        int i = 0;
+        do {
+            if (i < n) {
+                if (apples[i] == 0 && days[i] == 0) {
+                    ;
+                } else if (apples[i] != 0) {
+                    pq.offer(new int[]{apples[i], days[i] + i});
+                }
+            }
+            if (!pq.isEmpty()) {
+                int[] entry = null;
+                do {
+                    int[] p = pq.poll();
+                    if (i >= p[1]) continue;
+                    entry = p;
+                    break;
+                } while (!pq.isEmpty());
+                if (entry != null) {
+                    entry[0]--;
+                    result++;
+                    if (entry[0] > 0) pq.offer(entry);
+                }
+            }
+            i++;
+        } while (!pq.isEmpty() || i < n);
+        return result;
+    }
+}
