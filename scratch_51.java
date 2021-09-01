78f50e8118b8f322747cf29a6802e97978a894ca
gyakkun <xiaowufeixia@gmail.com>
2021-09-01 10:58:27 +0800

    new

--- scratch_51.java
+++ scratch_51.java
@@ -9,13 +9,105 @@ class Scratch {
         Scratch s = new Scratch();
         long timing = System.currentTimeMillis();
 
-//        System.out.println(s.comb(1, 1));
-//        System.out.println(s.comb(2, 1));
-//        System.out.println(s.comb(130, 20));
-        System.out.println(s.maxRectangle(new String[]{"cwsmqtaausjakauhujlpgjwpapsyunylpivxzbbdkxfxmeoypt", "n", "gd", "ib", "zj", "uw", "fg", "nr", "qp", "fb", "vq", "gz", "un", "tl", "gn", "bshvfxaadfogxjjgmjuwijdcpnluaztzyyzhwkkpsznvilhhcc", "bg", "zb", "qc", "rs", "jj", "khkxfuyqkfmrmqnxkbokatfkaplbdallpjdryysfrqbjuzatqf", "ju", "iq", "hx", "zkvygirllpckgxwctgiyzbcuffnqolmbghvzaejzpqckrmjzwu", "aj", "mn", "ad", "oa", "kr", "bs", "mh", "pe", "ce", "xn", "jc", "ha", "zk", "gf", "xw", "ku", "ll", "fn", "ii", "gw", "yu", "up", "xh", "lo", "bsjsjppqxtqwrsxeyamophripgayjuzupkbqmkqmeqjkilcubh", "ne", "z", "bv", "ja", "rv", "lh", "dq", "ve", "jk", "qu", "zw", "lw", "aw", "rm", "bb", "xi", "rq", "zt", "js", "xu", "kw", "ro", "sf", "by", "gb", "mu", "jz", "gi", "boquvjhgadilhsbxhjtlmmutmnuttxugmhafppojfgbxghabuy", "yp", "lg", "cu", "ct", "mw", "xk", "sl", "zf", "eh", "so", "sp", "kp", "qz", "ty", "dy", "dz", "jt", "ey", "yt", "ih", "iwdggfxtdgklqpxdsarqexjbgepkdbkmnvdzbmdudhpjbngdxp", "hf", "mx", "uouzqwjkwdzvqfdhzpfvzpeajfkvtxdkcrwhvbqbhlqshyaybh", "ef", "ir", "adktqiniltegtvohcunbbxwzpmypkgodihutzinwiufbegevqc", "bh", "dm", "fk", "uk", "ae", "nm", "qk", "toilgaddlgxypkpvfpkctfyiudjcbjpfedgjmvsfrenrvroxdi", "ao", "bthhjsqqrqjubfivllsydjmmwpjlymynhqpofwlayzxwksicuu", "tjecqwbnfgnpqaqkfkyailypqaubuqwcmhjimphfazbroagzyb", "xr", "gs", "r", "rn", "oiikbjjpjngxprbkkerocamygvghxwxboyxafyumexsyycsfcu", "sm", "ij", "fj", "ta", "fe", "ox", "rz", "cg", "vv", "cz", "pv", "uj", "em", "rl", "uv", "vy", "yzvaqlpeuafqfrgdxnsrtsizleobksnijtrvhcvjxzrvvwhlzj", "wl", "nu", "io", "bm", "lmkrzmfvhaidusnztyjdxchgthqabzerrnbysboyauwuiqgnrj", "pp", "whqplhrluofemarlwgvonyoivmkhaaugqfgifdpxyufaiikqaq", "cb", "ri", "ym", "lu", "pcgamtwwpqyokvdzimjmeompemhxewwjjmhywfinsjmmrrmnbi", "ncqfjjreumidraarkycprwzkdudpzkecyzkcyaxuuvhnfjfrtn", "to", "mr", "bycjuhplzcjrscepphwcccalrosgonyzgexlojtshileqceyyt", "sn", "xp", "kf", "ul", "pb", "iv", "ea", "ap", "su", "th", "kg", "ysukcysqdilqemxikiqynsifdeiblrvznedfrlalmzczqywmqv", "lj", "xz", "ra", "fo", "iy", "mm", "zu", "pwjpjxirlqswexleoeotuqudzpseowuqjihmbfmlepgquixppa", "ya", "dg", "lktvueuukxahguvplyocaoyqspfqvwgmrznztlaefxspicorpe", "dw", "nbznkghfiljhsvuiznshnzwkpuukikbzeyeuqrmthdrkncmjiv", "vr", "jh", "ly", "co", "bt", "bl", "wv", "er", "wd", "ye", "mi", "ny", "hy", "nz", "ip", "wk", "hn", "asaplbgmfnfaycpddojdcliwvuylsoiefsmkaazjzherdjbxus", "tr", "st", "sa", "ke", "ee", "wh", "px", "ws", "ng", "qflxhbcepgukmlsyeneiuvagdnpyallkepgniutddpkrhpsecr", "jl", "cy", "av", "pt", "hj", "rk", "kd", "gx", "kk", "esfpdztmjtnbglsslxvwkyrkrhwybbwhataverbwkqghupczvo", "qv", "vo", "xc", "gr", "ml", "uf", "hq", "hdhqghtxfhmrmdpmzdaylngtgoqznqaztchfanvumqdraxeixq", "ak", "bz", "yk", "rpuwhkpibuypilyabjnqnekaptlypkgxozbjvozptessyyvicr", "lb", "yx", "gqovsmbldqwfyroyyweeukudtxikqatyauguorjvgzrppjydtl", "da", "dn", "du", "atsbxpvmjtcernkghoznhgvslekssoigjnneooveoruaqgyytn", "qg", "li", "py", "tw", "dj", "if", "ns", "eq", "ni", "si", "tx", "as", "at", "sy", "jx", "in", "mp", "uzlwwhtvmfilekqrliftjgnqxnjrnsltcywobftyfsaxiazlqz", "xf", "zsnuzoiqeezwujyngvppjofnksppakejmlgrbknlpqhckeungv", "ux", "uu", "jhxynhixxbrrkufmtxdjikkevrvntsgngttgudondepizpnikd", "rf", "pj", "rc", "m", "nh", "zs", "am", "jd", "wg", "ms", "bw", "kx", "ka", "yn", "tg", "gj", "fi", "krstgziiqabggvvufvuszmsyiiaxdwzawrbozbrvafuefoglqs", "te", "aa", "jg", "nf", "yw", "yq", "zm", "nt", "en", "rucggdxdkynxyvkhvbyanotbtqbfzpzyavzbkahjgpfjllzyef", "d", "kj", "elhfspkxdxqnpmpkdcgmosmglcdcgvqwbrxfzmlaurtvaythjk", "kq", "vc", "ho", "wj", "xq", "ic", "ev", "wy", "tc", "fooqakmfhzcsotjjkozdbijsoetdwqiktlisrstduwlnmksslh", "uz", "el", "hw", "gp", "np", "ph", "be", "dd", "mo", "lc", "va", "tq", "vl", "ln", "az", "tz", "dkidytafkhhlbmdhzkgqmcvctbafroiitafzwoyoohkzhhikie", "v", "xx", "jf", "rfzndcdrshuillnjwrttsorsjqemazazpeifkoecqkvewbtbth", "us", "cj", "mv", "fl", "qj", "ck", "bk", "s", "ls", "wo", "htfmmbnstvtfixgolzkebgjxdtimcvnjiddkzokkuoebptxhuo", "rw", "mz", "vu", "it", "wn", "hz", "xmgsracbzttbwqpiwpflflhvmcjedvnrwxxptjbifdstrpipwv", "wm", "yb", "tu", "cd", "ds", "bu", "db", "gadtwqkazgwellbnmhirhcknahqxeiqgmdgumtujnkedlufyjs", "vlmqrilyscywjlytqzvogakjypnksnhhmovdgikqcbjtfescug", "t", "nc", "yr", "ch", "i", "rd", "eb", "pq", "xy", "do", "hu", "c", "kjxhwupoxpqtcgafzyxmceippseyqwowdgxacppmwljsreiqwk", "k", "vb", "df", "sq", "ou", "pc", "tn", "yg", "eg", "wt", "je", "mt", "vi", "ob", "wu", "os", "atlqerdjdszzmyqwutkcixujbnymgpjrssmadscnxokzbvalge", "ac", "xo", "cm", "za", "im", "gu", "jnwpdxbterxxgqsifspvhtjkrrjrzsbkmkjpbxgtvxghaeovem", "nb", "hp", "fhguuzpuricbknarvfvuuaiqjuoneprtedxhokkoomnpyzrffk", "ze", "oc", "xb", "o", "hc", "yy", "qw", "hv", "sc", "bx", "ci", "ax", "sx", "uh", "au", "ut", "ld", "y", "x", "eu", "kn", "go", "pz", "hk", "nl", "po", "mc", "ivhfelbyrrdjtxtdilqhhxpwyeuusgrrewzggafnkwqxdhwqnp", "lfprlmqoqyaidlggysrjvraskrfaihsvnlojvcxdkxwqsujmmc", "b", "aq", "ti", "ar", "vqtrekspbphuhdjmvbrbrsbjnwxwefsaujonueezvgsgwshctk", "jdjikdnrfuynqdenupurxwwxvdrenuwznduourgrkfzwxsucob", "p", "hjzxxhlvpmrdcgomiaydnmmkqyrawxhczldrdmqhzzmslpfzvz", "dr", "vs", "cv", "zg", "on", "yf", "pa", "gupimkaxzmfktkzdctinmhlimdfcxhipsmdoluodlbvkyhcmki", "flbabfhawdaqlwvpcstuikgcjkdqknfcmopnqtvpussgflmeqb", "f", "oy", "sr", "reoukvhpfgzkdsimjuifwmyrybmwmuoavkfdsijwlugjhyfimd", "xsqxmaanlrljzihrgpguewkbqksrzhgkonlbfpmudiubernymq", "kveomwtmyfyfnucdcphwqfmmmifhibnbogbwzazulbqbstbrzk", "h", "or", "tk", "xm", "ex", "og", "lt", "q", "e", "ykypmlotoztrgvwnpxuzhvawbkterpdbjcmtbamvgeahgzdgsq", "xa", "na", "zp", "qa", "ru", "sd", "gv", "zupodigekkqqpzxzgqvzziejimhrbokysmuhsafjpjbwktnuhl", "jjutatmjaydqkppqzmlvpgqpuowfkdqblyipivdvmzjrtskdlu", "iz", "yd", "sk", "vk", "rx", "hg", "gk", "hr", "ekciqajslpdscierruoyuguajljprqvtknplayirfsdjqfpidw", "om", "ub", "xd", "km", "ez", "he", "dx", "ig", "id", "pd", "qhegnofrraudcxwzdfxmheaersybvxpgocgsazukleuvafpmuj", "wa", "qt", "cw", "no", "lp", "wb", "ql", "yzkahsewrhfriyjbbuwnaaglygppsstpqfgifsfeakptocpsfj", "wi", "iu", "bo", "ge", "gh", "cf", "wf", "qemoqpltvoneqrgrmtouakbuonlcnfmfmheuicsasqwhmorwvf", "gm", "ie", "rh", "a", "pi", "rkilafbvqwenffovvgebfpnqdtqjxfqtmygaimsrxratvuqqxk", "nd", "pu", "vz", "uo", "rb", "fs", "yoprekcfrmetsdrkjinmhhdpkmcqmvtgkaqknvbpcbhgmsqehe", "opekcclbtawwhvemtmstzalhfkjhxbavgagjmlgwhqktcoosst", "bf", "wzsymzwxkbkgjxziqhxezkfyfmlmtcaphqxmfdlucocftlgkdj", "ud", "fq", "ur", "vd", "nq", "nzavtdsdxafxrihxsuaxnkcslohgevipbqivtpmiipaprvukfp", "fr", "bi", "obcrtojfysapvcpmfpchrypkvcwbfszaoivkbaoruftxblklmj", "zd", "oe", "nw", "dh", "yc", "jo", "vkgsqujvizliryrtqbugstjizernuxctirsvcfeqdkvlwpvsad", "jr", "br", "samdncvzklkbpqpkitzikypiiumlahfwsjacywxxidckgqnfgc", "ok", "fv", "rj", "ed", "qx", "oz", "bq", "fhlpatrkdqzpvcedjrjdutdcurwiencuimpkkjtcjwkoqtlpep", "uc", "tb", "ts", "es", "vg", "lx", "lv", "oi", "tt", "zr", "zo", "eriudaxijfznacglrhtxyyhkdhqzqqpcnofyrsddmioknzlerm", "jm", "jb", "od", "hm", "qy", "klmzkhdjqkvibelzkgzjdxzqeqznfpmerbezfhofbpsxuxqzrw", "ua", "vm", "mk", "hb", "zy", "pm", "iw", "dc", "la", "vp", "zl", "lqkylbwdmirzskccrhfwspqrpqbwedwgrjixgyiqtelguguepv", "zxhaiovtaskhvywxxxsvkwqujfmknklpondzwovuhwnszwkrvn", "jv", "fm", "ps", "wz", "jq", "sv", "vh", "nv", "pqovwnffwqsgdhnsckeqmbuhdlqwpkglwqbpbsrkxpfbanitsm", "ia", "jy", "vw", "lz", "nj", "uq", "hs", "wp", "qf", "se", "qe", "ab", "yi", "hhblsevefjnqeekzdyjuvubhifusxmqqngpcssobiabmjevvkp", "dp", "dfedkaivixvydogxxiqftbpzjesmjtilwitumfpxrjpmllymwe", "vf", "ol", "qn", "sw", "bc", "oq", "ky", "il", "tv", "xj", "zz", "eo", "vt", "bd", "md", "xg", "copaksgibhlhkpqxsugbseomulcglhkvgidgecmnemtfqcxcqb", "zq", "fp", "ff", "yv", "qo", "tm", "kv", "eqmjuybpsfxmbypqeovnmuhqsslparumvkvkrzkgomvgoanalt", "yj", "et", "adzxwpqbkythwjrjeqnjxtfiviyscvbymxquwhnkmjbwocxkzu", "pr", "lm", "lf", "ue", "xe", "lq", "kb", "kl", "ba", "cc", "nk", "nx", "sz", "jn", "ei", "nn", "uy", "an", "hi", "yz", "fa", "zi", "xt", "ji", "yh", "xl", "qm", "cr", "yl", "mq", "rt", "pl", "cp", "cs", "fz", "ug", "lr", "zikimyhqdalrwsxgagetoaofqlvmjlomwpkrnzurpmswuylhaq", "dqkswyrkfhjmezwxqvqwkbqjgqjfnwmmxrbfglmkmjbdzuzcsa", "oo", "lk", "tf", "pg", "cl", "yo", "ww", "vj", "u", "le", "ca", "kh", "gg", "mvgkxfembpprqvyantbskhbycclvnwmeskguxlmeceqraqpbci", "gq", "qb", "jw", "kc", "ej", "bp", "qq", "ov", "fh", "we", "vn", "jp", "ah", "me", "fx", "cx", "l", "ko", "wr", "tj", "fdrgfycjxzruwaanmyhaohwlfypvmkpqxzknhbpwbnwyhghzls", "mg", "my", "ziuibvhguhwziomtxbleugfaigpgmduvhfdxbvvvgeoagxsllp", "ai", "tbqtjiwwqftmhwwvauzzicybdvulksastulcquuokpqerpgqje", "gc", "zv", "taxyfiiyapfnqaifvahxmsgcorihsildysshgbxucxmogouwtl", "tfgeigfqowcgnoxyutsnsrymkoshyeeuzvluujkuiotpjopqgj", "bycdykpmtsryetqzsblkjfzuxwczpthgyclhlxhcmpevnrnmhp", "dl", "qi", "oh", "ht", "wx", "is", "sg", "wq", "ix", "of", "pk", "sh", "zwbenudxmnnseibttpybkxmakarngnkoeqdeifaxvfebaospkx", "zc", "hd", "bj", "hh", "vtzizanfmdkxklnuxloiqvsqplivyyvxvcpnucyoxedlacatwe", "gy", "ot", "oj", "fy", "pn", "mj", "dk", "vx", "fc", "pw", "kz", "ss", "ys", "ow", "ec", "al", "cq", "xv", "de", "zh", "w", "zn", "mb", "ay", "ek", "td", "cbnvjoixfeuupomhqjjktmfmfaawxkrveqvpvjkrdlptyellac", "kt", "qh", "fd", "ft", "ma", "qd", "dt", "wc", "zx", "szsuhmftaarehqcnsmtxnciptadokmohhguizcbxmuihxajnou", "rg", "ew", "qr", "rr", "ndqobdpgnjahrnaorbgrubqxpgiabnxwkymjvpgezlvsnvyxpe", "re", "tp", "ag", "ki", "pf", "gl", "ks", "fw", "gt", "ep", "j", "ga", "tvberqlfknpumimdglrjhoaocuyabqtznoewsderntuvdcpymd", "swbdzkzikrulrmxpiaukijfxlvrxmwdojsudqftfzlwehkeddd", "rp", "sb", "di", "um", "fu", "hl", "djgbkaciyeracnpljhpqhjbpdlqcdfhovlhaignnaihbfuiskr", "dv", "op", "g", "ui", "ry", "sj", "ik", "bn", "af", "mf", "cn", "xs", "qs"}));
+        TreeNode t0 = new TreeNode(0);
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        t0.left = t1;
+        t1.left = t2;
+        t2.left = t3;
+        System.out.println(s.longestConsecutiveLc549(t0));
 
         timing = System.currentTimeMillis() - timing;
         System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC549
+    int lc549MaxLen = 0;
+    Map<TreeNode, Integer> lc549DiCache;
+    Map<TreeNode, Integer> lc549DdCache;
+
+    public int longestConsecutiveLc549(TreeNode root) {
+        lc549DiCache = new HashMap<>();
+        lc549DdCache = new HashMap<>();
+        bothSide(root);
+        return lc549MaxLen;
+    }
+
+    private void bothSide(TreeNode root) {
+        // 左侧递增上来, 右侧递减下去
+        if (root == null) return;
+        int leftInc = 1, rightInc = 1, leftDec = 1, rightDec = 1, tmpMax = 1;
+        // 左边递增上来, 右边递减下去
+        if (root.left != null && root.left.val == root.val - 1) {
+            leftInc += dfsIncreasing(root.left);
+            if (root.right != null && root.right.val == root.val + 1) {
+                leftInc += dfsDecreasing(root.right);
+            }
+            tmpMax = Math.max(tmpMax, leftInc);
+        }
+
+        // 右边递增上来, 左边递减下去
+        if (root.right != null && root.right.val == root.val - 1) {
+            rightInc += dfsIncreasing(root.right);
+            if (root.left != null && root.left.val == root.val + 1) {
+                rightInc += dfsDecreasing(root.left);
+            }
+            tmpMax = Math.max(tmpMax, rightInc);
+        }
+
+        // 左边递减下去, 右边递增上来
+        if (root.left != null && root.left.val == root.val + 1) {
+            leftDec += dfsDecreasing(root.left);
+            if (root.right != null && root.right.val == root.val - 1) {
+                leftDec += dfsIncreasing(root.right);
+            }
+            tmpMax = Math.max(tmpMax, leftDec);
+        }
+
+        // 右边递减下去, 左边递增上来
+        if (root.right != null && root.right.val == root.val + 1) {
+            rightDec += dfsDecreasing(root.right);
+            if (root.left != null && root.left.val == root.val - 1) {
+                rightDec += dfsIncreasing(root.left);
+            }
+            tmpMax = Math.max(tmpMax, rightDec);
+        }
+
+
+        lc549MaxLen = Math.max(lc549MaxLen, tmpMax);
+        bothSide(root.left);
+        bothSide(root.right);
+    }
+
+    private int dfsIncreasing(TreeNode root) { // 从这一支递增上来, 也就是说子节点要刚好比本节点小1
+        if (root == null) return 0;
+        if (lc549DiCache.containsKey(root)) return lc549DiCache.get(root);
+        int left = 1, right = 1;
+        if (root.left != null && root.left.val == root.val - 1) {
+            left += dfsIncreasing(root.left);
+        }
+        if (root.right != null && root.right.val == root.val - 1) {
+            right += dfsIncreasing(root.right);
+        }
+        int result = Math.max(left, right);
+        lc549DiCache.put(root, result);
+        return result;
+    }
+
+    private int dfsDecreasing(TreeNode root) { // 从这一支递减上来, 也就是说子节点要比本节点大1
+        if (root == null) return 0;
+        if (lc549DdCache.containsKey(root)) return lc549DdCache.get(root);
+        int left = 1, right = 1;
+        if (root.left != null && root.left.val == root.val + 1) {
+            left += dfsDecreasing(root.left);
+        }
+        if (root.right != null && root.right.val == root.val + 1) {
+            right += dfsDecreasing(root.right);
+        }
+        int result = Math.max(left, right);
+        lc549DdCache.put(root, result);
+        return result;
     }
 
     // Interview 17.25 Hard 拒绝暴搜 反复剪枝
