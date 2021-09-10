2eb6bc5ad892a8e34b64ac4a413f6ab60d589b15
gyakkun <xiaowufeixia@gmail.com>
2021-09-10 15:21:07 +0800

    new

--- scratch_53.java
+++ scratch_53.java
@@ -11,6 +11,40 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC1953 TLE
+    public long numberOfWeeks(int[] milestones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(o -> -milestones[o]));
+        int prevWeek = -1, n = milestones.length;
+        for (int i = 0; i < n; i++) pq.offer(i);
+        int count = 0;
+        while (!pq.isEmpty()) {
+            if (prevWeek == -1) {
+                int thisWeek = pq.poll();
+                milestones[thisWeek]--;
+                prevWeek = thisWeek;
+            } else {
+                int thisWeek = pq.poll();
+                milestones[thisWeek]--;
+                if (milestones[prevWeek] != 0) pq.offer(prevWeek);
+                prevWeek = thisWeek;
+            }
+            count++;
+            // 检查
+            // if (pq.isEmpty()) break;
+        }
+        return count;
+    }
+
+
+    //    给你 n 个项目，编号从 0 到 n - 1 。同时给你一个整数数组 milestones ，其中每个 milestones[i] 表示第 i 个项目中的阶段任务数量。
+    //
+    //    你可以按下面两个规则参与项目中的工作：
+    //
+    //    每周，你将会完成 某一个 项目中的 恰好一个 阶段任务。你每周都 必须 工作。
+    //    在 连续的 两周中，你 不能 参与并完成同一个项目中的两个阶段任务。
+    //    一旦所有项目中的全部阶段任务都完成，或者仅剩余一个阶段任务都会导致你违反上面的规则，那么你将 停止工作 。注意，由于这些条件的限制，你可能无法完成所有阶段任务。
+    //
+    //    返回在不违反上面规则的情况下你 最多 能工作多少周。
 
     // LC249
     public List<List<String>> groupStrings(String[] strings) {
