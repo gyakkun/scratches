80a31d6ea396e0dd7f22ad1c7fa3210f36898f57
gyakkun <xiaowufeixia@gmail.com>
2021-09-10 15:07:10 +0800

    new

---
+++ scratch_53.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scratch s = new Scratch();
+        long timing = System.currentTimeMillis();
+
+        System.out.println(s.groupStrings(new String[]{"a", "a"}));
+
+        timing = System.currentTimeMillis() - timing;
+        System.err.println("TIMING: " + timing + "ms.");
+    }
+
+
+    // LC249
+    public List<List<String>> groupStrings(String[] strings) {
+        List<List<String>> result = new ArrayList<>();
+        Map<Integer, Map<String, Integer>> m = new HashMap<>();
+        for (String s : strings) {
+            m.putIfAbsent(s.length(), new HashMap<>());
+            Map<String, Integer> inner = m.get(s.length());
+            inner.put(s, inner.getOrDefault(s, 0) + 1);
+        }
+
+        for (Map<String, Integer> s : m.values()) {
+            while (!s.isEmpty()) {
+                String w = s.keySet().iterator().next();
+                // 构造
+                List<String> list = new ArrayList<>();
+                char[] ca = w.toCharArray();
+                for (int i = 0; i < 26; i++) {
+                    for (int j = 0; j < ca.length; j++) {
+                        ca[j] = (char) (((ca[j] - 'a' + 1) % 26) + 'a');
+                    }
+                    String built = new String(ca);
+                    if (s.containsKey(built)) {
+                        int count = s.get(built);
+                        s.remove(built);
+                        for (int j = 0; j < count; j++)
+                            list.add(built);
+                    }
+                }
+                result.add(list);
+            }
+        }
+        return result;
+    }
+}
