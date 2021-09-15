02a2e8b7d2bd3ae40ba3e4a39a7dda40a395d630
gyakkun <xiaowufeixia@gmail.com>
2021-09-15 10:44:19 +0800

    new

--- scratch_53.java
+++ scratch_53.java
@@ -22,8 +22,23 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
-    // Interview 10.09 LC240 两次二分 (不如双指针)
+    // Interview 10.09 双指针
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) return false;
+        if (matrix.length == 1 && matrix[0].length == 0) return false;
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchMatrixBS(int[][] matrix, int target) {
         if (matrix.length == 0) return false;
         if (matrix.length == 1 && matrix[0].length == 0) return false;
         int lo = 0, hi = matrix.length - 1;
