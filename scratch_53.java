2eead67b1c0c4729f34e084c66c6f429b70ca687
gyakkun <xiaowufeixia@gmail.com>
2021-09-15 09:22:54 +0800

    new

--- scratch_53.java
+++ scratch_53.java
@@ -21,6 +21,35 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC162
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 0;
+        if (n == 2) return nums[0] > nums[1] ? 0 : 1;
+        int lo = 0, hi = n - 1;
+        int result = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid == n - 1) {
+                if (nums[n - 1] > nums[n - 2]) return n - 1;
+                hi = mid - 1;  // 折返
+            } else if (mid == 0) {
+                if (nums[0] > nums[1]) return 0;
+                lo = mid + 1;  // 折返
+            } else {
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                    result = mid;
+                    break;
+                } else if (nums[mid] < nums[mid + 1]) { // 继续在右侧找
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return result;
+    }
+
     // LC699
     public List<Integer> fallingSquares(int[][] positions) {
         List<Integer> result = new ArrayList<>();
