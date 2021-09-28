28cc2c003652a64d85aa13a628c5c64016b06a71
gyakkun <xiaowufeixia@gmail.com>
2021-09-28 13:45:50 +0800

    new

--- scratch_54.java
+++ scratch_54.java
@@ -17,6 +17,23 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC1995
+    public int countQuadruplets(int[] nums) {
+        int n = nums.length, result = 0;
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+                for (int o = j + 1; o < n - 1; o++) {
+                    for (int p = o + 1; p < n; p++) {
+                        if (nums[i] + nums[j] + nums[o] == nums[p]) {
+                            result++;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     // JZOF II 021 LC19
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if (n <= 0) return head;
