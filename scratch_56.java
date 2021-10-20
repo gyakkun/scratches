77daf4434b76c58cdb109b16ac5dc302462f6514
gyakkun <xiaowufeixia@gmail.com>
2021-10-20 11:56:33 +0800

    new

--- scratch_56.java
+++ scratch_56.java
@@ -23,21 +23,24 @@ class Scratch {
 
     // LC1769
     public int[] minOperations(String boxes) {
-        // 有 n 个盒子。给你一个长度为 n 的二进制字符串 boxes ，其中 boxes[i] 的值为 '0' 表示第 i 个盒子是 空 的，而 boxes[i] 的值为 '1' 表示盒子里有 一个 小球。
-        //
-        //在一步操作中，你可以将 一个 小球从某个盒子移动到一个与之相邻的盒子中。第 i 个盒子和第 j 个盒子相邻需满足 abs(i - j) == 1 。注意，操作执行后，某些盒子中可能会存在不止一个小球。
-        //
-        //返回一个长度为 n 的数组 answer ，其中 answer[i] 是将所有小球移动到第 i 个盒子所需的 最小 操作数。
-        //
-        //每个 answer[i] 都需要根据盒子的 初始状态 进行计算。
         int n = boxes.length();
+        int leftOper = 0, leftOneCount = 0, rightOper = 0, rightOneCount = 0;
         int[] result = new int[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && boxes.charAt(j) == '1') {
-                    result[i] += Math.abs(i - j);
-                }
+            if (boxes.charAt(i) == '1') {
+                rightOneCount++;
+                rightOper += i;
             }
+        }
+        result[0] = rightOper;
+        for (int i = 1; i < n; i++) {
+            if (boxes.charAt(i - 1) == '1') {
+                leftOneCount++;
+                rightOneCount--;
+            }
+            leftOper += leftOneCount; // 每往左移一步, 左边的"1"到该点的路程各自加1, 总共加左边1的个数
+            rightOper -= rightOneCount; // 如果这个位置本身是1, 通过这里的减, 其实自身到自身的路程已经为0了
+            result[i] = leftOper + rightOper;
         }
         return result;
     }
