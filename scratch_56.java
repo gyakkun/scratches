8529bc2bf0477ce7f152f75778387c524d741883
gyakkun <xiaowufeixia@gmail.com>
2021-10-20 11:44:37 +0800

    new

--- scratch_56.java
+++ scratch_56.java
@@ -21,6 +21,27 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC1769
+    public int[] minOperations(String boxes) {
+        // 有 n 个盒子。给你一个长度为 n 的二进制字符串 boxes ，其中 boxes[i] 的值为 '0' 表示第 i 个盒子是 空 的，而 boxes[i] 的值为 '1' 表示盒子里有 一个 小球。
+        //
+        //在一步操作中，你可以将 一个 小球从某个盒子移动到一个与之相邻的盒子中。第 i 个盒子和第 j 个盒子相邻需满足 abs(i - j) == 1 。注意，操作执行后，某些盒子中可能会存在不止一个小球。
+        //
+        //返回一个长度为 n 的数组 answer ，其中 answer[i] 是将所有小球移动到第 i 个盒子所需的 最小 操作数。
+        //
+        //每个 answer[i] 都需要根据盒子的 初始状态 进行计算。
+        int n = boxes.length();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && boxes.charAt(j) == '1') {
+                    result[i] += Math.abs(i - j);
+                }
+            }
+        }
+        return result;
+    }
+
     // LCP33 **
     public int storeWater(int[] bucket, int[] vat) {
         int n = bucket.length, maxVat = Integer.MIN_VALUE;
