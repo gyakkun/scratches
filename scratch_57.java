0657f5b932bf4e07cf38e405ae05aee2a7f5af95
gyakkun <xiaowufeixia@gmail.com>
2021-10-26 14:09:50 +0800

    new

---
+++ scratch_57.java
@@ -0,0 +1,18 @@
+class Scratch {
+    public static void main(String[] args) {
+
+    }
+
+    // LC1844
+    public String replaceDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] ca = s.toCharArray();
+        for (int i = 0; i < ca.length; i++) {
+            if (i % 2 == 0) sb.append(ca[i]);
+            else sb.append((char) (ca[i - 1] + (ca[i] - '0')));
+        }
+        return sb.toString();
+    }
+
+    //
+}
