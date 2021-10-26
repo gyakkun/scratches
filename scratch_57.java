01a240745ad07fc79d694ce67bed8802c153d642
gyakkun <xiaowufeixia@gmail.com>
2021-10-26 14:45:55 +0800

    new

--- scratch_57.java
+++ scratch_57.java
@@ -1,6 +1,57 @@
 class Scratch {
     public static void main(String[] args) {
+        Scratch s = new Scratch();
+        long timing = System.currentTimeMillis();
 
+
+        System.out.println(s.numberOfArrays("1234567890", 90));
+
+
+        timing = System.currentTimeMillis() - timing;
+        System.err.println("TIMING: " + timing + "ms.");
+    }
+
+    // LC1266
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int x = points[0][0], y = points[0][1];
+        int result = 0;
+        for (int i = 1; i < points.length; i++) {
+            int nx = points[i][0], ny = points[i][1];
+            int deltaX = Math.abs(nx - x), deltaY = Math.abs(ny - y);
+            int slash = Math.min(deltaX, deltaY);
+            int line = Math.max(deltaX, deltaY) - slash;
+            result += line + slash;
+            x = nx;
+            y = ny;
+        }
+        return result;
+    }
+
+    // LC1416
+    Integer[] lc1416Memo;
+
+    public int numberOfArrays(String s, int k) {
+        int n = s.length();
+        lc1416Memo = new Integer[n + 1];
+        return lc1416Helper(0, s, k);
+    }
+
+    private int lc1416Helper(int cur, String s, int k) {
+        final long mod = 1000000007l;
+        if (cur == s.length()) return 1;
+        if (lc1416Memo[cur] != null) return lc1416Memo[cur];
+        int len = 1;
+        long result = 0;
+        while (cur + len <= s.length()) {
+            long num = Long.parseLong(s.substring(cur, cur + len));
+            if (String.valueOf(num).length() != len) break;
+            if (num > k) break;
+            if (num < 1) break;
+            result += lc1416Helper(cur + len, s, k);
+            result %= mod;
+            len++;
+        }
+        return lc1416Memo[cur] = (int) (result % mod);
     }
 
     // LC1844
