3d1163706e99e00414df29561b217b2f1c7a28c1
gyakkun <xiaowufeixia@gmail.com>
2021-10-29 17:12:48 +0800

    update

--- scratch_57.java
+++ scratch_57.java
@@ -19,8 +19,9 @@ class Scratch {
         // 偶数下标之和减奇数下标之和
         int n = nums.length;
         long[][] dp = new long[n + 2][2];
+        // dp[n][0/1] 表示选前n个数做子序列时候, 第n个数字下标作为偶/奇数时候的最大值
         for (int i = 0; i < n; i++) {
-            dp[i + 1][0] = Math.max(dp[i][0], dp[i][1] + nums[i]);
+            dp[i + 1][0] = Math.max(dp[i][0]/*不选这个数*/, dp[i][1] + nums[i]/*选这个数, 从上一个奇数结尾的状态转移过来*/);
             dp[i + 1][1] = Math.max(dp[i][1], dp[i][0] - nums[i]);
         }
         return Math.max(dp[n][0], dp[n][1]);
