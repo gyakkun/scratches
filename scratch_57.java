4b64d940cd314c0b9cd8cc010a6e618a8fb5d7cb
gyakkun <xiaowufeixia@gmail.com>
2021-11-02 10:54:33 +0800

    update

--- scratch_57.java
+++ scratch_57.java
@@ -930,18 +930,17 @@ class ListNode {
 class Lc1320 {
     static int[][] alphabetIdx;
     static int[][] distance;
+    static int[][] id;
     int min = Integer.MAX_VALUE;
-    int[][][] memo;
+    int[][] memo;
     char[] word;
 
 
     public int minimumDistance(String wordStr) {
         word = wordStr.toCharArray();
-        memo = new int[word.length][30][30];
+        memo = new int[word.length][31 * 31];
         for (int i = 0; i < word.length; i++) {
-            for (int j = 0; j < 30; j++) {
-                Arrays.fill(memo[i][j], -1);
-            }
+            Arrays.fill(memo[i], -1);
         }
         // 遍历两个手指的初始位置
         for (int i = 'A'; i <= '^'; i++) {
@@ -957,12 +956,11 @@ class Lc1320 {
         if (targetCharIdx == word.length) {
             return 0;
         }
-        if (memo[targetCharIdx][curLeft - 'A'][curRight - 'A'] != -1)
-            return memo[targetCharIdx][curLeft - 'A'][curRight - 'A'];
-        if (memo[targetCharIdx][curRight - 'A'][curLeft - 'A'] != -1)
-            return memo[targetCharIdx][curRight - 'A'][curLeft - 'A'];
+        if (memo[targetCharIdx][id[curLeft][curRight]] != -1)
+            return memo[targetCharIdx][id[curLeft][curRight]];
 
-        return memo[targetCharIdx][curLeft - 'A'][curRight - 'A'] = memo[targetCharIdx][curRight - 'A'][curLeft - 'A'] =
+
+        return memo[targetCharIdx][id[curLeft][curRight]] =
                 Math.min(distance[curLeft][word[targetCharIdx]] + helper(targetCharIdx + 1, word[targetCharIdx], curRight),
                         distance[curRight][word[targetCharIdx]] + helper(targetCharIdx + 1, curLeft, word[targetCharIdx]));
     }
@@ -970,6 +968,8 @@ class Lc1320 {
     static {
         alphabetIdx = new int[128][2];
         distance = new int[128][128];
+        id = new int[128][128];
+
         alphabetIdx['A'] = new int[]{0, 0};
         alphabetIdx['B'] = new int[]{0, 1};
         alphabetIdx['C'] = new int[]{0, 2};
@@ -1006,6 +1006,7 @@ class Lc1320 {
                 distance[i][j] = distance[j][i] =
                         Math.abs(alphabetIdx[i][0] - alphabetIdx[j][0])
                                 + Math.abs(alphabetIdx[i][1] - alphabetIdx[j][1]);
+                id[i][j] = id[j][i] = 31 * (i - 'A') + j - 'A';
             }
         }
     }
