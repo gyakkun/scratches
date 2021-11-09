f0dd05fb0887a08ec320a890a714709da3a0629d
gyakkun <xiaowufeixia@gmail.com>
2021-11-09 15:15:02 +0800

    new

--- scratch_58.java
+++ scratch_58.java
@@ -473,3 +473,82 @@ class TreeNode {
         val = x;
     }
 }
+
+// LC631
+class Excel {
+
+    Node[][] mtx;
+
+    public Excel(int height, char width) {
+        int h = height, w = width - 'A' + 1;
+        mtx = new Node[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                mtx[i][j] = new Node();
+            }
+        }
+    }
+
+    public void set(int row, char column, int val) {
+        int r = row - 1, c = column - 'A';
+        Node n = mtx[r][c];
+        n.val = val;
+        n.compose.clear();
+    }
+
+    public int get(int row, char column) {
+        int r = row - 1, c = column - 'A';
+        return mtx[r][c].eval();
+    }
+
+    public int sum(int row, char column, String[] numbers) {
+        int r = row - 1, c = column - 'A';
+        List<Node> compose = new ArrayList<>();
+        for (String q : numbers) {
+            if (q.indexOf(":") < 0) {
+                char icc = q.charAt(0);
+                int ic = icc - 'A';
+                int ir = Integer.valueOf(q.substring(1)) - 1;
+                compose.add(mtx[ir][ic]);
+            } else {
+                // 左上 : 右下
+                String[] arr = q.split(":");
+                int leftCol = arr[0].charAt(0) - 'A';
+                int leftRow = Integer.valueOf(arr[0].substring(1)) - 1;
+                int rightCol = arr[1].charAt(0) - 'A';
+                int rightRow = Integer.valueOf(arr[1].substring(1)) - 1;
+                for (int i = leftRow; i <= rightRow; i++) {
+                    for (int j = leftCol; j <= rightCol; j++) {
+                        compose.add(mtx[i][j]);
+                    }
+                }
+            }
+        }
+        mtx[r][c].compose = compose;
+        return mtx[r][c].eval();
+    }
+
+    private Node getNode(String q) {
+        char icc = q.charAt(0);
+        int ic = icc - 'A';
+        int ir = Integer.valueOf(q.substring(1)) - 1;
+        return mtx[ir][ic];
+    }
+
+
+    class Node {
+        int val = 0;
+        List<Node> compose = new ArrayList<>();
+
+        public int eval() {
+            if (compose.size() == 0) {
+                return val;
+            }
+            int result = 0;
+            for (Node n : compose) {
+                result += n.eval();
+            }
+            return result;
+        }
+    }
+}
