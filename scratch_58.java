c6d0526c52315c21c3389c36b1cb122a83a6b049
gyakkun <xiaowufeixia@gmail.com>
2021-11-09 15:45:02 +0800

    new

--- scratch_58.java
+++ scratch_58.java
@@ -12,6 +12,26 @@ class Scratch {
         System.err.println("TIMING: " + timing + "ms.");
     }
 
+    // LC1727 **
+    public int largestSubmatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != 0 && matrix[i - 1][j] != 0) {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            Arrays.sort(matrix[i]);
+            for (int j = n - 1; j >= 0; j--) {
+                result = Math.max(result, matrix[i][j] * (n - j));
+            }
+        }
+        return result;
+    }
+
     // LC1359 Hard ** 组合数学
     final long mod = 1000000007;
 
