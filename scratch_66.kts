6afbadd6311d4e1dba91dd471f13c0a4e98b21e4
gyakkun <xiaowufeixia@gmail.com>
2022-04-05 13:44:48 +0800

    new

--- scratch_66.kts
+++ scratch_66.kts
@@ -8,6 +8,13 @@ var after = Instant.now()!!
 System.err.println("TIMING: ${Duration.between(before, after).toMillis()}ms")
 
 class Solution {
+
+    // LC762
+    private val prime = setOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31)
+
+    fun countPrimeSetBits(left: Int, right: Int) = IntRange(left, right).count { it.countOneBits() in prime }
+
+
     // LC744
     fun nextGreatestLetter(letters: CharArray, target: Char): Char {
         return if (target >= letters.last()) letters.first() else {
