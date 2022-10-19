423a1f34eafcd97f7fd8726c23f9040be51ce625
gyakkun <xiaowufeixia@gmail.com>
2022-10-19 15:20:04 +0800

    new

--- scratch_72.java
+++ scratch_72.java
@@ -1,11 +1,52 @@
 import javafx.util.Pair;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 class Scratch {
     public static void main(String[] args) {
         Scratch s = new Scratch();
         System.err.println(s.largestArea(new String[]{"02520253", "51551213", "03512513", "34312132", "21051025", "52005131", "34235150", "22154013"}));
+    }
+
+    // LC1262 WA
+    public int maxSumDivThree(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 3 == 0) return sum;
+        Map<Integer, List<Integer>> m = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(i -> i % 3));
+        TreeSet<Integer> remain1 = new TreeSet<>(m.getOrDefault(1, new ArrayList<>()));
+        TreeSet<Integer> remain2 = new TreeSet<>(m.getOrDefault(2, new ArrayList<>()));
+        int minReduce = sum;
+        int targetRemain = sum % 3;
+        if (targetRemain == 1) {
+            if (remain1.size() >= 1) {
+                minReduce = Math.min(minReduce, remain1.first());
+            }
+            if (remain2.size() >= 2) {
+                Iterator<Integer> it = remain2.iterator();
+                int remain2First2Sum = 0;
+                for (int i = 0; i < 2; i++) {
+                    int tmp = it.next();
+                    remain2First2Sum += tmp;
+                }
+                minReduce = Math.min(minReduce, remain2First2Sum);
+            }
+        } else if (targetRemain == 2) {
+            if (remain2.size() >= 1) {
+                minReduce = Math.min(minReduce, remain2.first());
+            }
+            if (remain1.size() >= 2) {
+                Iterator<Integer> it = remain1.iterator();
+                int remain1First2Sum = 0;
+                for (int i = 0; i < 2; i++) {
+                    int tmp = it.next();
+                    remain1First2Sum += tmp;
+                }
+                minReduce = Math.min(minReduce, remain1First2Sum);
+            }
+        }
+        return sum - minReduce;
     }
 
     // LC2089
