b7aef73a0ea301f8157940ae761d0a890410bf21
gyakkun <xiaowufeixia@gmail.com>
2022-10-23 19:44:03 +0800

    new

--- scratch_72.java
+++ scratch_72.java
@@ -10,6 +10,22 @@ class Scratch {
         System.err.println(s.checkIfExist(new int[]{-2, 0, 10, -19, 4, 6, -8}));
     }
 
+    // LC1768
+    public String mergeAlternately(String word1, String word2) {
+        int n = Math.min(word1.length(), word2.length());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+        }
+        if (word1.length() > word2.length()) {
+            sb.append(word1.substring(n));
+        } else if (word2.length() > word1.length()) {
+            sb.append(word2.substring(n));
+        }
+        return sb.toString();
+    }
+
     // LC2156
     public String subStrHash(String s, int power, int modulo, int k, int hashValue) {
         long hashLong = (long) hashValue;
