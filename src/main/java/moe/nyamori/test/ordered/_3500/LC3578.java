6888788093dd176387b816c80c8eebd8681bea31
gyakkun <xiaowufeixia@gmail.com>
2025-12-06 20:15:54 +0800

    LC3578 Passed. Reference answer.

--- src/main/java/moe/nyamori/test/ordered/_3500/LC3578.java
+++ src/main/java/moe/nyamori/test/ordered/_3500/LC3578.java
@@ -1,7 +1,13 @@
 package moe.nyamori.test.ordered._3500;
 
-// Medium
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+// **Medium
 /**
  * 3578. Count Partitions With Max-Min Difference at Most K
  * You are given an integer array nums and an integer k. Your task is to partition nums into one or more non-empty contiguous segments such that in each segment, the difference between its maximum and minimum elements is at most k.
@@ -25,13 +31,21 @@ package moe.nyamori.test.ordered._3500;
  * Monotonic Queue
  */
 public class LC3578 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var sol = new LC3578();
 //        var nums = new int[]{9, 4, 1, 3, 7};
 //        var k = 4;
-        var nums = new int[]{3, 3, 4};
-        var k = 0;
+//        var nums = new int[]{3, 3, 4};
+//        var k = 0;
+        var k = 20_000_000;
+        var nums = Arrays.stream(
+                Files.readString(
+                        Path.of(System.getProperty("user.home"), "LC3578.txt")
+                ).trim().split(",")
+        ).mapToInt(Integer::parseInt).toArray();
+        var timing = System.currentTimeMillis();
         System.err.println(sol.countPartitions(nums, k));
+        System.err.println("Timing: " + (System.currentTimeMillis() - timing) + "ms");
     }
 
     static final long MOD = 1_000_000_007L;
@@ -39,41 +53,33 @@ public class LC3578 {
 
     public int countPartitions(int[] nums, int k) {
         var n = nums.length;
-        var memo = new Long[n + 1]; // nulls
-        /*var prefix = new Long[n + 1]; // nulls*/
-        return (int) (helper(n - 1, nums, k, memo) % MOD);
-    }
-
-    private long helper(int endIdx, int[] nums, int k, Long[] memo/*, Long[] prefix*/) {
-        if (endIdx <= 0) {
-            // a number subtracts itself always equals 0, and 0<=k, hence 1 should be returned
-            // ** counter-intuitive **
-            // For endIdx less than zero, we still treat it as an existing number ???
-            return 1;
-        }
-        if (memo[endIdx] != null) return memo[endIdx];
-        var res = 0L;
-        // find an L (lower bound), such that all sub ranges in [[L, endIdx], endIdx] satisfied the "k condition"
-        // var minDeq = new LinkedList<Integer>();
-        // var maxDeq = new LinkedList<Integer>();
-        // iterate from endIdx backward, until the max-min > k
-        var min = nums[endIdx];
-        var max = nums[endIdx];
-        var lowerBound = endIdx;
-        for (var i = endIdx - 1; i >= 0; i--) {
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
-            var diff = max - min;
-            if (diff <= k) {
-                lowerBound = i;
-            } else {
-                break;
+        var dp = new long[n + 1];
+        dp[0] = 1L;
+        var prefix = new long[n + 1]; // 左闭右开求和: SUM[i,j) = prefix[j] - prefix[i] -> 闭区间求和 SUM[i,j] = prefix[j+1] - prefix[i]
+        prefix[0] = 1L; // dp[i+1] = prefix[i] - prefix[lower_bound-1]
+        var minDeq = new LinkedList<Integer>(); // 单调队列 维护区间最值的**下标**
+        var maxDeq = new LinkedList<Integer>();
+        for (int i = 0, j = 0; i < n; i++) {
+            while (!maxDeq.isEmpty() && nums[maxDeq.peekLast()] <= nums[i]) {
+                maxDeq.pollLast();
             }
+            maxDeq.offerLast(i);
+            while (!minDeq.isEmpty() && nums[minDeq.peekLast()] >= nums[i]) {
+                minDeq.pollLast();
+            }
+            minDeq.offerLast(i);
+            // 滑动窗口
+            while (!maxDeq.isEmpty() && !minDeq.isEmpty() && nums[maxDeq.peekFirst()] - nums[minDeq.peekFirst()] > k) {
+                if (maxDeq.peekFirst() == j) maxDeq.pollFirst();
+                if (minDeq.peekFirst() == j) minDeq.pollFirst();
+                j++;
+            }
+
+            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + MOD) % MOD;
+            prefix[i + 1] = (prefix[i] + dp[i + 1]) % MOD;
         }
-        for (var i = lowerBound - 1; i < endIdx; i++) {
-            res += (helper(i, nums, k, memo) + MOD) % MOD;
-        }
-        return memo[endIdx] = res;
+
+        return (int) (dp[n] % MOD);
     }
 
 }
